2d6cd38fddbc3d99263316bdc6703a54d228f6a6
Devin Stein <devstein@seas.upenn.edu>
2022-11-06 08:34:58 -0800

    Add Supbase auth scaffolding

---
+++ app/src/lib/supabase/client.ts
@@ -0,0 +1,5 @@
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createClient } from '@supabase/auth-helpers-sveltekit';
+
+// https://github.com/supabase/auth-helpers/tree/main/packages/sveltekit
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
 	import '../app.css';
+
+	import { supabaseClient } from '$lib/supabase/client';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <slot />
