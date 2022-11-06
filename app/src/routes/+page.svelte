08d329ee60ecc634a1f45736ec0564e3c9c0c6c8
Devin Stein <devstein@seas.upenn.edu>
2022-11-06 08:34:58 -0800

    Add Supbase auth scaffolding

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="min-h-screen flex flex-col justify-center items-center">
-	<h1 class="text-6xl text-center pb-8">Welcome to Shared Recruiting Co</h1>
+	<h1 class="text-6xl text-center pb-8">Welcome to Shared Recruiting Co.</h1>
 	<button class="px-4 py-1 bg-gray-900 text-white rounded">Log In</button>
 </div>
