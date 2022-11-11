169a36d15bd1c8dd71512c211093c65a6a6b676c
Devin Stein <devstein@seas.upenn.edu>
2022-11-11 12:42:38 -0800

    chore: remove log statements

--- app/src/lib/supabase/client.ts
+++ app/src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-import { Database } from '$lib/supabase/types';
+// import type { Database } from '$lib/supabase/types';
 
 // https://github.com/supabase/auth-helpers/tree/main/packages/sveltekit
-export const supabaseClient = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
