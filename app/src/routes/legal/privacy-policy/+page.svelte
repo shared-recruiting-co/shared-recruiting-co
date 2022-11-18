3315f7c9f11a111b13074f925c9400e327432597
Devin Stein <devstein@seas.upenn.edu>
2022-11-18 15:16:31 -0800

    feat: add structure for markdown file for legal pages

---
+++ app/src/routes/legal/privacy-policy/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	import content from './privacy-policy.md?raw';
+</script>
+
+<div class="flex h-screen w-full flex-col items-center overflow-scroll">
+	<h1 class="my-4 text-center text-4xl sm:my-12 sm:text-5xl">Privacy Policy</h1>
+
+	<article class="prose pt-8">
+		{@html marked(content)}
+	</article>
+</div>
+
