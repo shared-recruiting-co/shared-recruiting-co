ee49ab04e56a8c51fbd5891d131931854800cb3b
Devin Stein <devstein@seas.upenn.edu>
2022-11-22 12:18:46 -0800

    chore: better comments

--- libs/gmail/fwd.go
+++ libs/gmail/fwd.go
@@ -8,16 +8,19 @@ import (
 )
 
 const (
+	// FwdMsgDelimiter is the delimiter used to separate the forwarded message from the original message.
 	FwdMsgDelimiter = "---------- Forwarded message ---------"
 )
 
-type ForwardedMessage struct {
+// ForwardMessage represents a message to be forwarded.
+type ForwardMessage struct {
 	Sender string
 	To     string
 	Parent *gmail.Message
 }
 
-func (m ForwardedMessage) GetParentHeader(name string) string {
+// GetParentHeader returns the value of the header with the given name from the parent message.
+func (m ForwardMessage) GetParentHeader(name string) string {
 	for _, h := range m.Parent.Payload.Headers {
 		if h.Name == name {
 			return h.Value
@@ -26,32 +29,28 @@ func (m ForwardedMessage) GetParentHeader(name string) string {
 	return ""
 }
 
-func (m ForwardedMessage) InReplyTo() string {
-	// The "In-Reply-To:" field will contain the contents of the "Message-
-	// ID:" field of the message to which this one is a reply (the "parent
-	// message").
-	// If there is more than one parent message, then the "In-
-	// Reply-To:" field will contain the contents of all of the parents'
-	// "Message-ID:" fields.
-	// If there is no "Message-ID:" field in any of
-	// the parent messages, then the new message will have no "In-Reply-To:"
-	// field.
+// InReplyTo returns the contents of the "In-Reply-To:" field of the message to which this one is a reply (the "parent message").
+// It follows the spec described in https://tools.ietf.org/html/rfc2822#section-3.6.4
+//
+// The "In-Reply-To:" field will contain the contents of the "Message-ID:" field of the message to which this one is a reply (the "parent message").
+// If there is more than one parent message, then the "In-Reply-To:" field will contain the contents of all of the parents' "Message-ID:" fields.
+// If there is no "Message-ID:" field in any of the parent messages, then the new message will have no "In-Reply-To:" field.
+func (m ForwardMessage) InReplyTo() string {
 	return m.GetParentHeader("Message-ID")
 }
 
-func (m ForwardedMessage) References() string {
-	// The "References:" field will contain the contents of the parent's
-	// "References:" field (if any) followed by the contents of the parent's
-	// "Message-ID:" field (if any).
-	// If the parent message does not contain
-	// a "References:" field but does have an "In-Reply-To:" field
-	// containing a single message identifier, then the "References:" field
-	// will contain the contents of the parent's "In-Reply-To:" field
-	// followed by the contents of the parent's "Message-ID:" field (if
-	// any).
-	// If the parent has none of the "References:", "In-Reply-To:",
-	// or "Message-ID:" fields, then the new message will have no
-	// "References:" field.
+// References returns the contents of the "References:" field of the message to which this one is a reply (the "parent message").
+// It follows the spec described in https://tools.ietf.org/html/rfc2822#section-3.6.4
+//
+// The "References:" field will contain the contents of the parent's "References:" field
+// (if any) followed by the contents of the parent's "Message-ID:" field (if any).
+// If the parent message does not contain a "References:" field
+// but does have an "In-Reply-To:" field containing a single message identifier, then the "References:" field
+// will contain the contents of the parent's "In-Reply-To:" field followed by
+// the contents of the parent's "Message-ID:" field (if any).
+// If the parent has none of the "References:", "In-Reply-To:", or "Message-ID:" fields,
+// then the new message will have no "References:" field.
+func (m ForwardMessage) References() string {
 
 	// references will always contain the messageID (if it exists)
 	references := m.GetParentHeader("Message-ID")
@@ -74,15 +73,17 @@ func (m ForwardedMessage) References() string {
 	return references
 }
 
-func (m ForwardedMessage) ParentBody() string {
+// ParentBody returns the body of the parent message.
+// Note: We always convert to text/plain. It's simpler to do deal with and is sufficient for our purposes.
+func (m ForwardMessage) ParentBody() string {
 	// The "Body:" field will contain the contents of the parent's
 	// "Body:" field (if any)
-	// Note: We always convert to text/plain. It's simpler to do deal with and is sufficient for our purposes
 	content, _ := getContentFromMessageParts(m.Parent.Payload)
 	return content
 }
 
-func (m ForwardedMessage) Raw() string {
+// Raw returns the raw RFC-822 compliant forwarded message.
+func (m ForwardMessage) Raw() string {
 	// New email headers
 	fwdHeaders := []gmail.MessagePartHeader{
 		{
@@ -138,7 +139,7 @@ func (m ForwardedMessage) Raw() string {
 		}
 	}
 	// add the delimiter
-	raw += fmt.Sprintf("\r\n%s\r\n", delimiter)
+	raw += fmt.Sprintf("\r\n%s\r\n", FwdMsgDelimiter)
 	// add the parent headers
 	for _, h := range parentHeaders {
 		// all headers should be defined
@@ -153,13 +154,17 @@ func (m ForwardedMessage) Raw() string {
 	return base64.URLEncoding.EncodeToString([]byte(raw))
 }
 
-func (m ForwardedMessage) Create() *gmail.Message {
+// Create a send-able gmail message from a forwarded message
+func (m ForwardMessage) Create() *gmail.Message {
 	return &gmail.Message{
 		Raw:      m.Raw(),
 		ThreadId: m.Parent.ThreadId,
 	}
 }
 
+// ForwardEmail is a good enough implementation of forwarding an email in the same format as the gmail client.
+// It is good enough because it doesn't naively handle HTML mime-type content or when there are multiple parent messages.
+// This is sufficient for our purposes.
 func ForwardEmail(srv *gmail.Service, userID string, messageID, to string) error {
 	// 1. get the original message
 	msg, err := srv.Users.Messages.Get(userID, messageID).Do()
@@ -174,7 +179,7 @@ func ForwardEmail(srv *gmail.Service, userID string, messageID, to string) error
 	}
 
 	// 3. Create the forwarded message
-	fwd := ForwardedMessage{
+	fwd := ForwardMessage{
 		Sender: profile.EmailAddress,
 		To:     to,
 		Parent: msg,
