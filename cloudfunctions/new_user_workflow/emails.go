12a3e2a08b9bc314ab67a2a00b6bcf08f46bc4e2
Devin Stein <devstein@seas.upenn.edu>
2022-11-26 13:26:22 -0800

    feat: exclude already labeled emails from historic search

--- cloudfunctions/new_user_workflow/emails.go
+++ cloudfunctions/new_user_workflow/emails.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	mail "github.com/shared-recruiting-co/shared-recruiting-co/libs/gmail"
 	"google.golang.org/api/gmail/v1"
 )
 
@@ -46,7 +47,7 @@ func GetNewEmailsSince(srv *gmail.Service, userID string, historyID uint64, labe
 func GetEmailsSinceLastYear(srv *gmail.Service, userID, pageToken string) ([]*gmail.Message, string, error) {
 	// get the date from one year ago, even archived messages (ignore deleted)
 	oneYearAgo := time.Now().AddDate(-1, 0, 0).Format("2006/01/02")
-	q := fmt.Sprintf("-label:sent after:%s", oneYearAgo)
+	q := fmt.Sprintf("-label:sent -label:%s after:%s", mail.SRC_JobOpportunityLabel, oneYearAgo)
 
 	m, err := srv.Users.Messages.List(userID).Q(q).PageToken(pageToken).MaxResults(maxResults).Do()
 
