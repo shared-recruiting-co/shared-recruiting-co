181c10b50e02637ead9d70f0217c6a94875c074b
Devin Stein <devstein@seas.upenn.edu>
2022-11-26 07:44:37 -0800

    feat: script for updating go mods across packages

---
+++ scripts/update-go-mods.sh
@@ -0,0 +1,11 @@
+pkgs=("./cloudfunctions/collect_examples" "./cloudfunctions/email_push_notifications" "./cloudfunctions/new_user_workflow" "./cloudfunctions/watch_emails")
+
+for pkg in "${pkgs[@]}"
+do
+  pushd $pkg
+  echo "Updating $(basename $pkg)"
+  go get github.com/shared-recruiting-co/shared-recruiting-co/libs/gmail@main
+  go get github.com/shared-recruiting-co/shared-recruiting-co/libs/db@main
+  go mod tidy
+  popd
+done
