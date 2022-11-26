c7d9a26420a7189c0517ad1ecca88591fb7cd520
Devin Stein <devstein@seas.upenn.edu>
2022-11-26 08:39:46 -0800

    feat: always ignore sent emails when collecting examples

--- cloudfunctions/collect_examples/emails.go
+++ cloudfunctions/collect_examples/emails.go
@@ -12,13 +12,14 @@ const maxResults = 250
 func GetSRCEmails(srv *gmail.Service, userID string, labelId string, startDate time.Time, pageToken string) ([]*gmail.Message, string, error) {
 	builder := srv.Users.Messages.List(userID).LabelIds(labelId).PageToken(pageToken).MaxResults(maxResults)
 
+	// always ignore sent emails
+	q := "-label:sent"
 	if !startDate.IsZero() {
 		// start the search from the start date
-		q := fmt.Sprintf("after:%s", startDate.Format("2006/01/02"))
-		builder.Q(q)
+		q = fmt.Sprintf("%s after:%s", q, startDate.Format("2006/01/02"))
 	}
 
-	m, err := builder.Do()
+	m, err := builder.Q(q).Do()
 
 	return m.Messages, m.NextPageToken, err
 }
