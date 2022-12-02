4d2eb9ec589a04fa0535e2549cd5785e6fd8f36c
Devin Stein <devstein@seas.upenn.edu>
2022-12-01 16:58:53 -0800

    chore: remove unused code

--- cloudfunctions/new_user_workflow/emails.go
+++ cloudfunctions/new_user_workflow/emails.go
@@ -9,37 +9,6 @@ import (
 )
 
 const maxResults = 250
-const historyTypeMessageAdded = "messageAdded"
-const defaultLabelID = "INBOX"
-
-func GetNewEmailsSince(srv *gmail.Service, userID string, historyID uint64, labelID string, pageToken string) ([]*gmail.Message, string, error) {
-	if labelID == "" {
-		labelID = defaultLabelID
-	}
-
-	r, err := srv.Users.History.
-		List(userID).
-		LabelId(labelID).
-		StartHistoryId(historyID).
-		HistoryTypes(historyTypeMessageAdded).
-		PageToken(pageToken).
-		MaxResults(maxResults).
-		Do()
-
-	if err != nil {
-		return nil, "", err
-	}
-
-	messages := []*gmail.Message{}
-	for _, h := range r.History {
-		// only look at messages added
-		for _, m := range h.MessagesAdded {
-			messages = append(messages, m.Message)
-		}
-	}
-
-	return messages, r.NextPageToken, nil
-}
 
 // GetEmailsSinceLastYear syncs all emails from the previous year to today.
 // It returns the messages fetched, the next page token, and an error.
