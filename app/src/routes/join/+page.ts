7d13147cae9d95fca60872bc2de6faa7e416954d
Devin Stein <devstein@seas.upenn.edu>
2022-12-06 10:00:03 -0800

    feat: redirect user if they are not logged in

--- app/src/routes/join/+page.ts
+++ app/src/routes/join/+page.ts
@@ -1,18 +1,16 @@
 import type { PageLoad } from './$types';
-// import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-// import { redirect } from '@sveltejs/kit';
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageLoad = async (event) => {
-	// const { session } = await getSupabase(event);
+	const { session } = await getSupabase(event);
 
-	// TODO: Alternatively, I can show a login button
-//
-	// // require user to be logged in
-	// if (!session) {
-		// throw redirect(303, '/');
-	// }
+	// require user to be logged in
+	if (!session) {
+		throw redirect(303, '/login');
+	}
 
-	// TODO: check if user is already off the waitlist
+	// TODO: check if user is already off the waitlist or has a profile
 
 	// do nothing
 	return {};
