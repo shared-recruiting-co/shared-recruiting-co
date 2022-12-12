8bb5908f636a214120686196e5a6338d4e2b08d6
Devin Stein <devstein@seas.upenn.edu>
2022-12-12 10:04:49 -0800

    fix: use correct uri for google svg

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -25,9 +25,9 @@
 	<p class="my-4">Sign into SRC with the email you typically receive recruiting inbound on.</p>
 	<div class="mt-12">
 		<button
-			class="flex items-center rounded-md bg-white px-2 py-2 text-slate-500 shadow"
+			class="flex items-center rounded-md bg-white px-2 py-2 text-slate-500 shadow hover:bg-slate-50"
 			on:click={handleLogin}
-			><img src="google.svg" alt="Google logo" class="mr-3" />Sign in with Google</button
+			><img src="/google.svg" alt="Google logo" class="mr-3" />Sign in with Google</button
 		>
 	</div>
 </div>
