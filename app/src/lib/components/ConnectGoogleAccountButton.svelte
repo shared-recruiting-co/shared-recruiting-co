cb29cd57bcfef60a1946fbfb92cb2d5603c76396
Devin Stein <devstein@seas.upenn.edu>
2022-12-12 15:20:06 -0800

    feat: basic account creation page

--- app/src/lib/components/ConnectGoogleAccountButton.svelte
+++ app/src/lib/components/ConnectGoogleAccountButton.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { PUBLIC_GOOGLE_CLIENT_ID, PUBLIC_GOOGLE_REDIRECT_URI } from '$env/static/public';
+	import { 
+		PUBLIC_GOOGLE_CLIENT_ID, 
+	} from '$env/static/public';
 
 	type OAuth2CallbackResponse = {
 		code: string;
@@ -28,6 +30,7 @@
 						// success
 						console.log('success');
 						// TODO: redirect
+						// TODO: show cool success message
 					} else {
 						console.log('error');
 						// log error
@@ -46,10 +49,9 @@
 		const client = window.google.accounts.oauth2.initCodeClient({
 			client_id: PUBLIC_GOOGLE_CLIENT_ID,
 			scope: 'https://www.googleapis.com/auth/gmail.modify',
-			// what about mobile??
-			// ux_mode: 'popup',
-			ux_mode: 'redirect',
-			redirect_uri: PUBLIC_GOOGLE_REDIRECT_URI,
+			// TODO: On mobile, default to redirect
+			// ux_mode: 'redirect',
+			// redirect_uri: PUBLIC_GOOGLE_REDIRECT_URI,
 			access_type: 'offline',
 			auto_select: true,
 			approval_prompt: 'auto',
@@ -68,10 +70,13 @@
 
 <button
 	disabled={!loaded}
-	class="flex items-center rounded-md bg-white px-2 py-2 text-slate-500 shadow hover:bg-slate-50 disabled:cursor-wait"
+	class="flex items-center rounded-md bg-white px-3 py-2 text-slate-500 shadow hover:bg-slate-50 disabled:cursor-wait"
 	on:click|preventDefault={connectAccount}
 	><img src="/google.svg" alt="Google logo" class="mr-3" />Connect Account</button
 >
+{#if error}
+	<p class="mt-1 text-rose-500 text-sm">{error}</p>
+{/if}
 <svelte:head>
 	<script src="/scripts/gsi.js" on:load={onLoad} async defer></script>
 </svelte:head>
