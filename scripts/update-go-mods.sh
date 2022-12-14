8738173be8996441ced69a2e4af05566592a6ced
Devin Stein <devstein@seas.upenn.edu>
2022-12-14 11:05:00 -0800

    feat: always use latest main revision when updating go mods

--- scripts/update-go-mods.sh
+++ scripts/update-go-mods.sh
@@ -1,11 +1,12 @@
 pkgs=("./cloudfunctions/collect_examples" "./cloudfunctions/email_push_notifications" "./cloudfunctions/full_email_sync" "./cloudfunctions/watch_emails")
+sha=$(git rev-parse origin/main)
 
 for pkg in "${pkgs[@]}"
 do
   pushd $pkg
   echo "Updating $(basename $pkg)"
-  go get -u github.com/shared-recruiting-co/shared-recruiting-co/libs/gmail@main
-  go get -u github.com/shared-recruiting-co/shared-recruiting-co/libs/db@main
+  go get -u github.com/shared-recruiting-co/shared-recruiting-co/libs/gmail@${sha}
+  go get -u github.com/shared-recruiting-co/shared-recruiting-co/libs/db@${sha}
   go mod tidy
   popd
 done
