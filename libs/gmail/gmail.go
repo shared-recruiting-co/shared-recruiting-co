4706affd465c60d7d95372a5608d491ea26146f2
Devin Stein <devstein@seas.upenn.edu>
2022-12-14 07:34:47 -0800

    feat: support creating a gmail service with the default client credentials

--- libs/gmail/gmail.go
+++ libs/gmail/gmail.go
@@ -3,6 +3,7 @@ package gmail
 import (
 	"context"
 	"encoding/json"
+	"net/http"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -26,4 +27,24 @@ func NewGmailService(ctx context.Context, creds []byte, auth []byte) (*gmail.Ser
 	client := config.Client(ctx, tok)
 
 	return gmail.NewService(ctx, option.WithHTTPClient(client))
+}
+
+// NewDefaultGmailService creates a new gmail service with default credentials on the local machine.
+// The default credentials for service accounts do not have a client secret, so they will not auto-refresh the token.
+// The auth token must be valid for the lifetime of the service.
+func NewDefaultGmailService(ctx context.Context, auth []byte) (*gmail.Service, error) {
+	tok := &oauth2.Token{}
+	err := json.Unmarshal(auth, tok)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ts, err := google.DefaultTokenSource(ctx, scopes...)
+	trans := &oauth2.Transport{
+		Source: oauth2.ReuseTokenSource(tok, ts),
+		Base:   http.DefaultTransport,
+	}
+	httpClient := &http.Client{Transport: trans}
+	return gmail.NewService(ctx, option.WithHTTPClient(httpClient))
 }
