0b9dba39f8e5fa9bd1c182d4a96ae90f47042f9e
Devin Stein <devstein@seas.upenn.edu>
2022-12-14 17:07:04 -0800

    feat: helper func for sending message via gmail api

--- app/src/lib/server/google/gmail.ts
+++ app/src/lib/server/google/gmail.ts
@@ -34,3 +34,30 @@ export const stop = async (accessToken: string) =>
 					Authorization: `Bearer ${accessToken}`
 				}
 		  });
+
+interface SendMessageArgs {
+	from: string;
+	to: string;
+	subject: string;
+	body: string;
+}
+
+const formatRawEmailMessage = ({ from, to, subject, body }: SendMessageArgs) =>
+	`From: ${from}\r\nTo: ${to}\r\nSubject: ${subject}\r\n\r\n${body}`;
+
+const urlSafeBase64 = (str: string) =>
+	Buffer.from(str).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+
+// sendMessage  is a simple wrapper around the gmail API to send an email message.
+// It does not handle replies, attachments, or other advanced features.
+export const sendMessage = async (accessToken: string, message: SendMessageArgs) => {
+	const raw = formatRawEmailMessage(message);
+	return await fetch('https://gmail.googleapis.com/gmail/v1/users/me/messages/send', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${accessToken}`
+		},
+		body: JSON.stringify({ raw: urlSafeBase64(raw) })
+	});
+};
