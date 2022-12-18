8490ff97918d9aff987b418febf2d02f1d56fa31
Devin Stein <devstein@seas.upenn.edu>
2022-12-17 18:06:51 -0800

    style: add more y margin

--- app/src/routes/account/profile/+page.svelte
+++ app/src/routes/account/profile/+page.svelte
@@ -61,7 +61,7 @@
 	const debouncedHandleInput = debounce(handleInput, debounceDelay);
 </script>
 
-<div class="my-12 lg:grid lg:grid-cols-12 lg:gap-x-5">
+<div class="my-12 sm:my-18 lg:grid lg:grid-cols-12 lg:gap-x-5">
 	<!-- Empty space for now -->
 	<aside class="block py-6 px-2 sm:px-6 lg:col-span-2 lg:py-0 lg:px-0" />
 	<div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
