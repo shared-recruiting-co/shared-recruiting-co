8c33ccebcef7548eb61b878a4be658816c93468b
Devin Stein <devstein@seas.upenn.edu>
2022-12-19 09:56:23 -0800

    feat: clone message from one inbox to another

---
+++ libs/gmail/clone.go
@@ -0,0 +1,23 @@
+package gmail
+
+import (
+	"google.golang.org/api/gmail/v1"
+)
+
+// CloneMessage is a convenience function to cloning a message from one inbox to another.
+// On success, it will return the new destination message.
+// Cloning differs from forwarding because it preserves the original message sender, recipient, and all other headers.
+func CloneMessage(src *Service, dst *Service, messageID string, dstLabelIds []string) (*gmail.Message, error) {
+	// Get the raw message from the source account
+	msg, err := src.Users.Messages.Get(src.UserID, messageID).Format("raw").Do()
+	if err != nil {
+		return nil, err
+	}
+	// Clear the source specific message properties
+	msg.Id = ""
+	msg.ThreadId = ""
+	// Set the destination labels
+	msg.LabelIds = dstLabelIds
+	// Insert the message into the destination account
+	return dst.Users.Messages.Insert(dst.UserID, msg).Do()
+}
