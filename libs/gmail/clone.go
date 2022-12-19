0b20a1adf708ec77bc85aca8dc7c3c7f6e13719f
Devin Stein <devstein@seas.upenn.edu>
2022-12-19 11:11:58 -0800

    chore: comment about anonymization support

--- libs/gmail/clone.go
+++ libs/gmail/clone.go
@@ -7,6 +7,7 @@ import (
 // CloneMessage is a convenience function to cloning a message from one inbox to another.
 // On success, it will return the new destination message.
 // Cloning differs from forwarding because it preserves the original message sender, recipient, and all other headers.
+// TODO: Support recipient anonymization (i.e. randomize 'To' header, strings.Replace first and last name).
 func CloneMessage(src *Service, dst *Service, messageID string, dstLabelIds []string) (*gmail.Message, error) {
 	// Get the raw message from the source account
 	msg, err := src.Users.Messages.Get(src.UserID, messageID).Format("raw").Do()
