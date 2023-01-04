96334570554775508c83c6cbb3d157c2fe67ec40
Devin Stein <devstein@seas.upenn.edu>
2023-01-04 12:19:21 -0800

    refactor: move current header/footer to only marketing pages

---
+++ app/src/routes/(marketing)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<body class="flex min-h-screen flex-col text-slate-900">
+	<Header />
+	<main class="flex-1">
+		<slot />
+	</main>
+	<Footer />
+</body>
