a7c4b186555406b60331cf81b8ce19e226c79433
Devin Stein <devstein@seas.upenn.edu>
2023-01-04 14:58:02 -0800

    refactor: move header/footer to marketing group

--- app/src/routes/(marketing)/+layout.svelte
+++ app/src/routes/(marketing)/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
-<body class="flex min-h-screen flex-col text-slate-900">
-	<Header />
-	<main class="flex-1">
-		<slot />
-	</main>
-	<Footer />
-</body>
+<Header />
+<main class="flex-1">
+	<slot />
+</main>
+<Footer />
