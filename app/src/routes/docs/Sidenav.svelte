2761c3b5a7a7099c05cb892bde5c658297199214
Devin Stein <devstein@seas.upenn.edu>
2023-01-05 10:17:01 -0800

    fix: update routes in sidenav

--- app/src/routes/docs/Sidenav.svelte
+++ app/src/routes/docs/Sidenav.svelte
@@ -5,18 +5,18 @@
 		{
 			title: 'Introduction',
 			links: [
-				{ title: 'Welcome', href: '/' },
-				{ title: 'Open Source', href: '/docs/installation' },
-				{ title: 'Security & Privacy', href: '/docs/security' }
+				{ title: 'Welcome', href: '/docs/welcome' },
+				{ title: 'Open Source', href: '/docs/open-source' },
+				{ title: 'Security & Privacy', href: '/docs/security-privacy' }
 			]
 		},
 		{
 			title: 'Core Concepts',
 			links: [
-				{ title: 'Connect Gmail Account', href: '/docs/connect-email' },
+				{ title: 'Connect Email', href: '/docs/connect-email' },
 				{
-					title: 'Gmail Labels',
-					href: '/docs/labels'
+					title: 'Email Labels',
+					href: '/docs/email-labels'
 				},
 				{ title: 'Email Settings', href: '/docs/email-settings' }
 			]
@@ -24,17 +24,19 @@
 		{
 			title: 'Contributing',
 			links: [
-				{ title: 'Community', href: '/docs/how-to-contribute' },
-				{ title: 'Code', href: '/docs/architecture-guide' },
-				{ title: 'Architecture', href: '/docs/architecture-guide' },
-				{ title: 'Data', href: '/docs/design-principles' }
+				{ title: 'Community', href: '/docs/contributing' },
+				{ title: 'Code', href: '/docs/contributing#code' },
+				{ title: 'Architecture', href: '/docs/contributing#architecture' },
+				{ title: 'Data', href: '/docs/contributing#email' }
 			]
 		}
 	];
 </script>
 
-<aside>
-	<nav class="text-base lg:text-sm">
+<aside
+	class="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto overflow-x-hidden py-16 pl-0.5"
+>
+	<nav class="w-64 pr-8 text-base lg:text-sm xl:w-72 xl:pr-16">
 		<ul class="space-y-9">
 			{#each nav as section}
 				<li>
@@ -48,11 +50,11 @@
 							<li class="relative">
 								<a
 									href={link.href}
-									class={'block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full ' +
-										link.href ===
-									$page.url.pathname
-										? 'font-semibold text-sky-500 before:bg-sky-500'
-										: 'text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300'}
+									class={`block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full ${
+										link.href === $page.url.pathname
+											? 'font-semibold text-sky-500 before:bg-sky-500'
+											: 'text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300'
+									}`}
 								>
 									{link.title}
 								</a>
