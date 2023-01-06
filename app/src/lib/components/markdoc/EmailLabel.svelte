50ed39652bad4e558dc10d5ca71b1c37e06d0239
Devin Stein <devstein@seas.upenn.edu>
2023-01-06 12:02:48 -0800

    style: use darker blue

--- app/src/lib/components/markdoc/EmailLabel.svelte
+++ app/src/lib/components/markdoc/EmailLabel.svelte
@@ -5,7 +5,7 @@
 
 <span
 	class="inline-flex items-center rounded-md px-1.5 py-0.5 text-sm font-medium text-white"
-	class:bg-blue-500={color === 'blue'}
+	class:bg-blue-600={color === 'blue'}
 	class:bg-red-600={color === 'red'}
 	class:bg-green-600={color === 'green'}
 >
