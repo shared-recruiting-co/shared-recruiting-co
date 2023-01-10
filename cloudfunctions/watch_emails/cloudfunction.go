13ff186876e29faf7f319d6aca4f8c1f39f9920b
Devin Stein <devstein@seas.upenn.edu>
2023-01-09 16:26:51 -0800

    feat: add sentry for errors in watch emails job

--- cloudfunctions/watch_emails/cloudfunction.go
+++ cloudfunctions/watch_emails/cloudfunction.go
@@ -2,11 +2,14 @@ package cloudfunctions
 
 import (
 	"encoding/base64"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/getsentry/sentry-go"
 
 	"google.golang.org/api/gmail/v1"
 
@@ -27,13 +30,38 @@ func jsonFromEnv(env string) ([]byte, error) {
 	return decoded, err
 }
 
+// generic error handler
+func handleError(w http.ResponseWriter, msg string, err error) {
+	err = fmt.Errorf("%s: %w", msg, err)
+	log.Print(err.Error())
+	sentry.CaptureException(err)
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
 func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 	log.Println("received watch trigger")
 	ctx := r.Context()
+
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Printf("sentry.Init: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+	defer sentry.RecoverWithContext(ctx)
+
 	creds, err := jsonFromEnv("GOOGLE_OAUTH2_CREDENTIALS")
 	if err != nil {
-		log.Printf("error getting credentials: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		handleError(w, "error getting credentials", err)
 		return
 	}
 
@@ -45,6 +73,7 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 	// TODO
 	// v0 -> no pagination, no go routines
 	// 2. Spawn a goroutine for each user to watch their emails
+	// https://docs.sentry.io/platforms/go/concurrency/
 	// 3. Wait for all goroutines to finish
 
 	// 1. Fetch valid auth tokens for all users
@@ -54,8 +83,7 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 	})
 
 	if err != nil {
-		log.Printf("error getting user tokens: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		handleError(w, "error getting user tokens", err)
 		return
 	}
 
@@ -71,7 +99,9 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 
 		srv, err = mail.NewService(ctx, creds, auth)
 		if err != nil {
-			log.Printf("error creating gmail service: %v", err)
+			err = fmt.Errorf("error creating gmail service: %w", err)
+			log.Print(err)
+			sentry.CaptureException(err)
 			hasError = true
 			continue
 		}
@@ -80,7 +110,8 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 		// This also keeps the user's refresh token valid for deactivated emails
 		gmailProfile, err := srv.Profile()
 		if err != nil {
-			log.Printf("error getting gmail profile: %v", err)
+			err = fmt.Errorf("error getting gmail profile: %w", err)
+			log.Print(err)
 
 			// check for oauth token expiration or revocation
 			if mail.IsOAuth2Error(err) {
@@ -98,6 +129,7 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 					log.Printf("marked user oauth token as invalid")
 				}
 			}
+			sentry.CaptureException(err)
 			hasError = true
 			continue
 		}
@@ -105,7 +137,9 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 		// validate the user's email is active
 		userProfile, err := queries.GetUserProfileByEmail(ctx, gmailProfile.EmailAddress)
 		if err != nil {
-			log.Printf("error getting user profile: %v", err)
+			err = fmt.Errorf("error getting user profile: %w", err)
+			log.Print(err)
+			sentry.CaptureException(err)
 			hasError = true
 			continue
 		}
@@ -125,7 +159,9 @@ func runWatchEmails(w http.ResponseWriter, r *http.Request) {
 		})
 
 		if err != nil {
-			log.Printf("error watching: %v", err)
+			err = fmt.Errorf("error watching email: %w", err)
+			log.Print(err)
+			sentry.CaptureException(err)
 			continue
 		}
 		// success
