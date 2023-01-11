e2490ed3c61ee0896dfd7e268c4deec90206d817
Devin Stein <devstein@seas.upenn.edu>
2023-01-11 10:31:27 -0800

    style: job table ui

--- app/src/routes/(app)/account/jobs/+page.ts
+++ app/src/routes/(app)/account/jobs/+page.ts
@@ -2,7 +2,20 @@ import type { PageLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async (event) => {
+type Job = {
+	job_id: string;
+	company: string;
+	role: string;
+	recruiter: string;
+	recruiter_email: string;
+	emailed_at: string;
+};
+
+type Data = {
+	jobs: Job[];
+};
+
+export const load: PageLoad<Data> = async (event) => {
 	const { session } = await getSupabase(event);
 
 	// require user to be logged in
@@ -10,5 +23,16 @@ export const load: PageLoad = async (event) => {
 		throw redirect(303, '/login');
 	}
 
-	return {};
+	return {
+		jobs: [
+			{
+				job_id: '1',
+				company: 'Shared Recruiting Co.',
+				role: 'Founding Engineer',
+				recruiter: 'Devin Stein',
+				recruiter_email: 'devin@sharedrecruiting.co',
+				emailed_at: new Date().toLocaleDateString()
+			}
+		]
+	};
 };
