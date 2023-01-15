14976258dd42076399ce5df235a3fe10b5909220
Devin Stein <devstein@seas.upenn.edu>
2023-01-15 14:38:01 -0800

    feat: add description and empty state

--- app/src/routes/(app)/account/jobs/+page.svelte
+++ app/src/routes/(app)/account/jobs/+page.svelte
@@ -61,17 +61,18 @@
 	const pages = getPages(data.pagination.page, data.pagination.numPages);
 
 	// TOOD:
-	// Description
 	// Error state (icon + No jobs + Description)
 	// Empty state
-	// Realtime?
+	// Realtime
 	// move get pages to utils
 </script>
 
 <div>
 	<h1 class="text-3xl sm:text-4xl">Jobs</h1>
-	<p class="mt-1 text-sm text-slate-500">Coming soon!</p>
+	<p class="mt-1 text-sm text-slate-500 max-w-3xl">Reivew your inbound job opportunity history.</p>
+	<p class="mt-2 text-sm text-slate-500 max-w-3xl"> Inbound recruiting emails are automatically parsed and added to your job board. Only emails that describe the company and role will be added to your job board.</p>
 </div>
+{#if data.jobs && data.jobs.length > 0 }
 <div>
 	<div class="mt-8 overflow-hidden rounded-lg shadow ring-1 ring-black ring-opacity-5 md:mx-0">
 		<table class="min-w-full divide-y divide-slate-300">
@@ -125,7 +126,7 @@
 		<div
 			class="flex items-center justify-between border-t border-slate-200 bg-white px-4 py-3.5 sm:px-6"
 		>
-			<div class="flex flex-1 justify-end sm:hidden">
+			<div class="flex flex-1 justify-end md:hidden">
 				<a
 					href="#"
 					class="relative inline-flex items-center rounded-md border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-50"
@@ -137,15 +138,15 @@
 					>Next</a
 				>
 			</div>
-			<div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+			<div class="hidden md:flex sm:flex-1 sm:items-center sm:justify-between">
 				<div>
 					<p class="text-sm text-slate-700">
 						Showing
-						<span class="font-medium">{showingStart}</span>
+						<span class="font-semibold">{showingStart}</span>
 						to
-						<span class="font-medium">{showingEnd}</span>
+						<span class="font-semibold">{showingEnd}</span>
 						of
-						<span class="font-medium">{data.pagination.numResults}</span>
+						<span class="font-semibold">{data.pagination.numResults}</span>
 						results
 					</p>
 				</div>
@@ -156,7 +157,7 @@
 							class:focus:z-20={data.pagination.hasPrev}
 							class:hover:bg-slate-50={data.pagination.hasPrev}
 							class:cursor-not-allowed={!data.pagination.hasPrev}
-							class="relative inline-flex items-center rounded-l-md border border-slate-300 bg-white px-2 py-2 text-sm font-medium text-slate-500"
+							class="relative inline-flex items-center rounded-l-md border border-slate-300 bg-white px-2 py-2 text-sm font-semibold text-slate-500"
 						>
 							<span class="sr-only">Previous</span>
 							<!-- Heroicon name: mini/chevron-left -->
@@ -223,3 +224,14 @@
 		</div>
 	</div>
 </div>
+{:else}
+	<!-- Empty state -->
+	<div class="flex-1 flex flex-col justify-center items-center py-20">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-slate-500">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+</svg>
+
+		<p class="mt-2 text-base font-medium">No jobs found.</p>
+		<p class="mt-4 text-sm slate-500 max-w-xs text-center">Once your job emails are processed they will appear in your job board.</p>
+	</div>
+{/if}
