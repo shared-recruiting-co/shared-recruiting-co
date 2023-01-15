fd9c62f0d3528c113dcacd3a2eb0c5e3b58534f4
Devin Stein <devstein@seas.upenn.edu>
2023-01-15 14:42:04 -0800

    Populate Jobs Table from DB (#81)
    * feat: pagination on jobs
    
    * feat: add description and empty state
    
    * fix: remove comment

--- app/src/routes/(app)/account/jobs/+page.svelte
+++ app/src/routes/(app)/account/jobs/+page.svelte
@@ -2,12 +2,76 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const showingStart = (data.pagination.page - 1) * data.pagination.perPage + 1;
+	const showingEnd = Math.min(
+		data.pagination.page * data.pagination.perPage,
+		data.pagination.numResults
+	);
+	const prevPage = data.pagination.hasPrev ? data.pagination.page - 1 : data.pagination.page;
+	const nextPage = data.pagination.hasNext ? data.pagination.page + 1 : data.pagination.page;
+
+	const getPages = (current: number, total: number): string[] => {
+		if (total <= 5) {
+			return Array.from({ length: total }, (_, i) => (i + 1).toString());
+		}
+		switch (current) {
+			case 1:
+				return ['1', '2', '...', (total - 1).toString(), total.toString()];
+			case 2:
+				return ['1', '2', '3', '...', (total - 1).toString(), total.toString()];
+			case 3:
+				// special case for 6
+				if (total === 6) {
+					return ['1', '2', '3', '4', '5', '6'];
+				}
+				return ['1', '2', '3', '4', '...', (total - 1).toString(), total.toString()];
+			case total - 2:
+				// special case for 6
+				if (total === 6) {
+					return ['1', '2', '3', '4', '5', '6'];
+				}
+				return [
+					'1',
+					'2',
+					'...',
+					(total - 3).toString(),
+					(total - 2).toString(),
+					(total - 1).toString(),
+					total.toString()
+				];
+			case total - 1:
+				return ['1', '2', '...', (total - 2).toString(), (total - 1).toString(), total.toString()];
+			case total:
+				return ['1', '2', '...', (total - 1).toString(), total.toString()];
+			default:
+				return [
+					'1',
+					'2',
+					...(current - 1 == 3 ? [] : ['...']),
+					(current - 1).toString(),
+					current.toString(),
+					(current + 1).toString(),
+					...(current + 1 == total - 2 ? [] : ['...']),
+					(total - 1).toString(),
+					total.toString()
+				];
+		}
+	};
+	const pages = getPages(data.pagination.page, data.pagination.numPages);
+
+	// TOOD:
+	// Error state (icon + No jobs + Description)
+	// Realtime
+	// move get pages to utils
 </script>
 
 <div>
 	<h1 class="text-3xl sm:text-4xl">Jobs</h1>
-	<p class="mt-1 text-sm text-slate-500">Coming soon!</p>
+	<p class="mt-1 text-sm text-slate-500 max-w-3xl">Reivew your inbound job opportunity history.</p>
+	<p class="mt-2 text-sm text-slate-500 max-w-3xl"> Inbound recruiting emails are automatically parsed and added to your job board. Only emails that describe the company and role will be added to your job board.</p>
 </div>
+{#if data.jobs && data.jobs.length > 0 }
 <div>
 	<div class="mt-8 overflow-hidden rounded-lg shadow ring-1 ring-black ring-opacity-5 md:mx-0">
 		<table class="min-w-full divide-y divide-slate-300">
@@ -58,5 +122,115 @@
 				{/each}
 			</tbody>
 		</table>
+		<div
+			class="flex items-center justify-between border-t border-slate-200 bg-white px-4 py-3.5 sm:px-6"
+		>
+			<div class="flex flex-1 justify-end md:hidden">
+				<a
+					href="#"
+					class="relative inline-flex items-center rounded-md border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-50"
+					>Previous</a
+				>
+				<a
+					href="#"
+					class="relative ml-3 inline-flex items-center rounded-md border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-50"
+					>Next</a
+				>
+			</div>
+			<div class="hidden md:flex sm:flex-1 sm:items-center sm:justify-between">
+				<div>
+					<p class="text-sm text-slate-700">
+						Showing
+						<span class="font-semibold">{showingStart}</span>
+						to
+						<span class="font-semibold">{showingEnd}</span>
+						of
+						<span class="font-semibold">{data.pagination.numResults}</span>
+						results
+					</p>
+				</div>
+				<div>
+					<nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+						<a
+							href="/account/jobs?page={prevPage}"
+							class:focus:z-20={data.pagination.hasPrev}
+							class:hover:bg-slate-50={data.pagination.hasPrev}
+							class:cursor-not-allowed={!data.pagination.hasPrev}
+							class="relative inline-flex items-center rounded-l-md border border-slate-300 bg-white px-2 py-2 text-sm font-semibold text-slate-500"
+						>
+							<span class="sr-only">Previous</span>
+							<!-- Heroicon name: mini/chevron-left -->
+							<svg
+								class="h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</a>
+						{#each pages as page}
+							{@const current = page === `${data.pagination.page}`}
+							{#if page === '...'}
+								<span
+									class="relative inline-flex items-center border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-700"
+									>...</span
+								>
+							{:else}
+								<a
+									href="/account/jobs?page={page}"
+									class="relative inline-flex items-center border border-slate-300 bg-white px-4 py-2 text-sm font-medium text-slate-500 focus:z-20"
+									class:hover:bg-blue-50={!current}
+									class:z-10={current}
+									class:bg-blue-50={current}
+									class:border-blue-500={current}
+									class:text-blue-600={current}>{page}</a
+								>
+							{/if}
+						{/each}
+						<a
+							href="/account/jobs?page={nextPage}"
+							disabled={!data.pagination.hasNext}
+							class="relative inline-flex items-center rounded-r-md border border-slate-300 bg-white px-2 py-2 text-sm font-medium text-slate-500"
+							class:focus:z-20={data.pagination.hasNext}
+							class:hover:bg-slate-50={data.pagination.hasNext}
+							class:cursor-not-allowed={!data.pagination.hasNext}
+						>
+							<span class="sr-only">Next</span>
+							<!-- Heroicon name: mini/chevron-right -->
+							<svg
+								class="h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</a>
+					</nav>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
+{:else}
+	<!-- Empty state -->
+	<div class="flex-1 flex flex-col justify-center items-center py-20">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-slate-500">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+</svg>
+
+		<p class="mt-2 text-base font-medium">No jobs found.</p>
+		<p class="mt-4 text-sm slate-500 max-w-xs text-center">Once your job emails are processed they will appear in your job board.</p>
+	</div>
+{/if}
--- app/src/routes/(app)/account/jobs/+page.ts
+++ app/src/routes/(app)/account/jobs/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import { redirect } from '@sveltejs/kit';
+import { redirect, error } from '@sveltejs/kit';
 
 type Job = {
 	job_id: string;
@@ -11,28 +11,75 @@ type Job = {
 	recruiter_email: string;
 };
 
+type Pagination = {
+	page: number;
+	perPage: number;
+	numPages: number;
+	numResults: number;
+	hasNext: boolean;
+	hasPrev: boolean;
+};
+
 type Data = {
 	jobs: Job[];
+	pagination: Pagination;
 };
 
-const defaultJob: Job = {
-	job_id: '1',
-	company: 'Shared Recruiting Co.',
-	job_title: 'Founding Engineer',
-	recruiter: 'Devin Stein',
-	recruiter_email: 'devin@sharedrecruiting.co',
-	emailed_at: new Date().toLocaleDateString()
-};
+const PAGE_SIZE = 10;
 
 export const load: PageLoad<Data> = async (event) => {
-	const { session } = await getSupabase(event);
+	const { session, supabaseClient } = await getSupabase(event);
 
 	// require user to be logged in
 	if (!session) {
 		throw redirect(303, '/login');
 	}
 
+	// get the query parameters from the URL (default to 1)
+	const page = parseInt(event.url.searchParams.get('page') || '1') || 1;
+	const start = (page - 1) * PAGE_SIZE;
+	const stop = start + PAGE_SIZE;
+
+	// get jobs from database
+	const { data: jobs, error: jobsError } = await supabaseClient
+		.from('user_email_job')
+		.select('*')
+		.order('emailed_at', { ascending: false })
+		.range(start, stop);
+
+	// TODO: decide on error handling
+	if (jobsError) {
+		console.error(jobsError);
+		throw error(500, jobsError.message);
+	}
+
+	const { count, error: countError } = await supabaseClient.from('user_email_job').select('*', {
+		head: true,
+		count: 'exact'
+	});
+
+	if (countError || !count) {
+		console.error(countError);
+		throw error(500, countError.message);
+	}
+
+	const formattedJobs = jobs.map(({ data, ...job }) => ({
+		...job,
+		emailed_at: new Date(job.emailed_at).toLocaleDateString(),
+		// TS is complaining because jsonb can technically be string, number, boolean, or JSON, or JSON[]
+		recruiter: data?.recruiter,
+		recruiter_email: data?.recruiter_email
+	}));
+
 	return {
-		jobs: [defaultJob]
+		jobs: formattedJobs,
+		pagination: {
+			page,
+			perPage: PAGE_SIZE,
+			numPages: Math.ceil(count / PAGE_SIZE),
+			numResults: count,
+			hasPrev: page > 1,
+			hasNext: page < Math.ceil(count / PAGE_SIZE)
+		}
 	};
 };
