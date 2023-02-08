d5de88b65e4cd2ef37de4f33cf87fad86f40e250
Devin Stein <devstein@seas.upenn.edu>
2023-02-08 15:40:59 -0800

    refactor: move form helpers into shared file

---
+++ app/src/lib/forms.ts
@@ -0,0 +1,19 @@
+import type { FormData } from './$types';
+
+export const isValidUrl = (str: string): boolean => {
+	try {
+		new URL(str);
+		return true;
+	} catch (error) {
+		// If the URL is invalid, an error will be thrown
+		return false;
+	}
+};
+
+export const getTrimmedFormValue = (data: FormData, key: string): string => {
+	const value = data.get(key);
+	if (typeof value === 'string') {
+		return value.trim();
+	}
+	return '';
+};
