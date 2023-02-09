5a76499b894ae7703ceebb29e84284527eee757d
Devin Stein <devstein@seas.upenn.edu>
2023-02-09 14:04:58 -0500

    refactor: share overly complicated pagination logic

--- app/src/routes/(app)/account/jobs/+page.svelte
+++ app/src/routes/(app)/account/jobs/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import { getPaginationPages } from '$lib/pagination';
+
 	export let data: PageData;
 
 	$: showingStart = (data.pagination.page - 1) * data.pagination.perPage + 1;
@@ -11,54 +13,7 @@
 	$: prevPage = data.pagination.hasPrev ? data.pagination.page - 1 : data.pagination.page;
 	$: nextPage = data.pagination.hasNext ? data.pagination.page + 1 : data.pagination.page;
 
-	const getPages = (current: number, total: number): string[] => {
-		if (total <= 5) {
-			return Array.from({ length: total }, (_, i) => (i + 1).toString());
-		}
-		switch (current) {
-			case 1:
-				return ['1', '2', '...', (total - 1).toString(), total.toString()];
-			case 2:
-				return ['1', '2', '3', '...', (total - 1).toString(), total.toString()];
-			case 3:
-				// special case for 6
-				if (total === 6) {
-					return ['1', '2', '3', '4', '5', '6'];
-				}
-				return ['1', '2', '3', '4', '...', (total - 1).toString(), total.toString()];
-			case total - 2:
-				// special case for 6
-				if (total === 6) {
-					return ['1', '2', '3', '4', '5', '6'];
-				}
-				return [
-					'1',
-					'2',
-					'...',
-					(total - 3).toString(),
-					(total - 2).toString(),
-					(total - 1).toString(),
-					total.toString()
-				];
-			case total - 1:
-				return ['1', '2', '...', (total - 2).toString(), (total - 1).toString(), total.toString()];
-			case total:
-				return ['1', '2', '...', (total - 1).toString(), total.toString()];
-			default:
-				return [
-					'1',
-					'2',
-					...(current - 1 == 3 ? [] : ['...']),
-					(current - 1).toString(),
-					current.toString(),
-					(current + 1).toString(),
-					...(current + 1 == total - 2 ? [] : ['...']),
-					(total - 1).toString(),
-					total.toString()
-				];
-		}
-	};
-	$: pages = getPages(data.pagination.page, data.pagination.numPages);
+	$: pages = getPaginationPages(data.pagination.page, data.pagination.numPages);
 
 	// TOOD:
 	// Error state (icon + No jobs + Description)
