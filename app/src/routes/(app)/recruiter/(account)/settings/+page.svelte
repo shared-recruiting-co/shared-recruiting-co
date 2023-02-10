2da3e92f84db3ef760a66eceb14ed4dc309f12f8
Devin Stein <devstein@seas.upenn.edu>
2023-02-10 18:27:00 -0500

    feat: let recruiters connect email (no ux)

--- app/src/routes/(app)/recruiter/(account)/settings/+page.svelte
+++ app/src/routes/(app)/recruiter/(account)/settings/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import ConnectGoogleAccountButton from '$lib/components/ConnectGoogleAccountButton.svelte';
+
+	export let data: PageData;
+
+	const onConnect = () => {
+		console.log('connected');
+		// do nothing for now
+	};
+</script>
+
 <div>Coming soon.</div>
+
+<ConnectGoogleAccountButton {onConnect} email={data.session?.user?.email} />
