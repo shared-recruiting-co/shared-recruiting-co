f89d1dfb946da7d98717e76622c9c3f031350437
Devin Stein <devstein@seas.upenn.edu>
2023-02-15 18:07:00 -0500

    refactor: use struct to pass state in push notification handler

--- cloudfunctions/email_push_notifications/sync.go
+++ cloudfunctions/email_push_notifications/sync.go
@@ -2,7 +2,6 @@ package cloudfunctions
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -25,64 +24,21 @@ type FullEmailSyncRequest struct {
 	StartDate time.Time `json:"start_date"`
 }
 
-func publishMessages(email string, messages []*gmail.Message) {
-	if len(messages) == 0 {
-		return
-	}
-
-	ctx := context.Background()
-	// push message to be processed
-	emailMessages := schema.EmailMessages{
-		Email:    email,
-		Messages: make([]string, len(messages)),
-		Settings: schema.EmailMessagesSettings{},
-	}
-	for i, message := range messages {
-		emailMessages.Messages[i] = message.Id
-	}
-	projectID := os.Getenv("GCP_PROJECT_ID")
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		// TODO: Handle error.
-		log.Printf("failed to create pubsub client: %v", err)
-	}
-	topic := client.Topic(os.Getenv("CANDIDATE_GMAIL_MESSAGES_TOPIC"))
-	defer topic.Stop()
-
-	rawMessage, err := json.Marshal(emailMessages)
-	if err != nil {
-		log.Printf("failed to marshal email messages: %v", err)
-	}
-
-	// publish message
-	result := topic.Publish(ctx, &pubsub.Message{
-		Data: rawMessage,
-	})
-
-	// Block until the result is returned and a server-generated ID is returned
-	// for the published message.
-	id, err := result.Get(ctx)
-	if err != nil {
-		log.Printf("failed to publish message: %v", err)
-	}
-	log.Printf("Published a message; msg ID: %v", id)
-}
-
 // triggerBackgroundfFullEmailSync triggers a background function to sync all emails since a given date
 // Note: The service account must be a principal with invoker permission on the full sync service
-func triggerBackgroundfFullEmailSync(ctx context.Context, email string, startDate time.Time) error {
+func (cf *CloudFunction) triggerBackgroundfFullEmailSync(startDate time.Time) error {
 	triggerURL := os.Getenv("TRIGGER_FULL_SYNC_URL")
 	if triggerURL == "" {
 		return errors.New("TRIGGER_FULL_SYNC_URL is not set")
 	}
-	httpClient, err := idtoken.NewClient(ctx, triggerURL)
+	httpClient, err := idtoken.NewClient(cf.ctx, triggerURL)
 	if err != nil {
 		return err
 	}
 
 	// trigger full sync
 	body := FullEmailSyncRequest{
-		Email:     email,
+		Email:     cf.payload.Email,
 		StartDate: startDate,
 	}
 	bodyBytes, err := json.Marshal(body)
@@ -93,7 +49,7 @@ func triggerBackgroundfFullEmailSync(ctx context.Context, email string, startDat
 	// trigger in go routine, so that the function can return and the cloudfunction can be scaled down
 	// the proper way to do this is to use pubsub for automatic retries, but this is a quick and dirty solution
 	go func() {
-		log.Printf("trigger full sync for %s from %s", email, startDate.Format("2006/01/02"))
+		log.Printf("trigger full sync for %s from %s", cf.payload.Email, startDate.Format("2006/01/02"))
 		_, err = httpClient.Post(triggerURL, "application/json", io.NopCloser(bytes.NewReader(bodyBytes)))
 		if err != nil {
 			log.Printf("failed to trigger full sync: %v", err)
@@ -103,29 +59,30 @@ func triggerBackgroundfFullEmailSync(ctx context.Context, email string, startDat
 	return nil
 }
 
-func syncNewEmails(
-	user db.UserProfile,
-	srv *srcmail.Service,
+func (cf *CloudFunction) syncHistory(
 	syncHistory db.UserEmailSyncHistory,
 ) error {
 	var err error
 	pageToken := ""
 	var messages []*gmail.Message
+	var results []*pubsub.PublishResult
 	historyIDExpired := false
+
+	defer cf.topics.CandidateGmailMessages.Stop()
 
 	for {
 		// Make request to fetch new emails from previous history id or last sync date
 		// get next set of messages
 		if historyIDExpired {
 			// if history id is expired, trigger async full sync to last sync date
-			err = triggerBackgroundfFullEmailSync(context.Background(), user.Email, syncHistory.SyncedAt)
+			err = cf.triggerBackgroundfFullEmailSync(syncHistory.SyncedAt)
 			if err != nil {
 				return fmt.Errorf("error triggering full sync: %v", err)
 			}
 			// done!
 			return nil
 		} else {
-			messages, pageToken, err = fetchNewEmailsSinceHistoryID(srv, uint64(syncHistory.HistoryID), "UNREAD", pageToken)
+			messages, pageToken, err = fetchNewEmailsSinceHistoryID(cf.srv, uint64(syncHistory.HistoryID), pageToken)
 		}
 
 		// for now, abort on error
@@ -141,14 +98,49 @@ func syncNewEmails(
 			return fmt.Errorf("error fetching emails: %v", err)
 		}
 
-		// TODO: Publish all messages before waiting on results
-		// var results []*pubsub.PublishResult
-		publishMessages(user.Email, messages)
+		if len(messages) > 0 {
+			result, err := cf.PublishCandidateMessages(messages)
+			if err != nil {
+				return fmt.Errorf("error publishing candidate messages: %w", err)
+			}
+
+			results = append(results, result)
+		}
 
 		if pageToken == "" {
 			break
 		}
 	}
 
+	// wait for all messages to be processed
+	for _, result := range results {
+		_, err := result.Get(cf.ctx)
+		if err != nil {
+			// log but do not abort
+			log.Printf("error getting publish result: %v", err)
+		}
+	}
+
 	return nil
+}
+
+func (cf *CloudFunction) PublishCandidateMessages(messages []*gmail.Message) (*pubsub.PublishResult, error) {
+	emailMessages := schema.EmailMessages{
+		Email:    cf.payload.Email,
+		Messages: make([]string, len(messages)),
+	}
+	for i, message := range messages {
+		emailMessages.Messages[i] = message.Id
+	}
+	rawMessage, err := json.Marshal(emailMessages)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling email messages: %w", err)
+	}
+
+	// publish message
+	result := cf.topics.CandidateGmailMessages.Publish(cf.ctx, &pubsub.Message{
+		Data: rawMessage,
+	})
+
+	return result, nil
 }
