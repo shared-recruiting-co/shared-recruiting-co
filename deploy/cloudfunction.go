3b4121eb9903a574764109560169c431422f58ca
Devin Stein <devstein@seas.upenn.edu>
2023-02-15 17:14:32 -0500

    feat: grant push notification handler permission to push to recruiter topic

--- deploy/cloudfunction.go
+++ deploy/cloudfunction.go
@@ -265,6 +265,20 @@ func (i *Infra) emailPushNotificationCF(fullSync *CloudFunction) (*CloudFunction
 		return nil, err
 	}
 
+	_, err = pubsub.NewTopicIAMMember(i.ctx, fmt.Sprintf("%s-publish-to-recruiter-gmail-messages", name), &pubsub.TopicIAMMemberArgs{
+		Topic:   i.Topics.RecruiterGmailMessages.ID(),
+		Role:    pulumi.String("roles/pubsub.publisher"),
+		Member:  pulumi.Sprintf("serviceAccount:%s", sa.Email),
+		Project: pulumi.String(*i.Project.ProjectId),
+	}, pulumi.DependsOn([]pulumi.Resource{
+		cf,
+		sa,
+		i.Topics.RecruiterGmailMessages,
+	}))
+	if err != nil {
+		return nil, err
+	}
+
 	return &CloudFunction{
 		Name:           name,
 		ServiceAccount: sa,
@@ -274,7 +288,6 @@ func (i *Infra) emailPushNotificationCF(fullSync *CloudFunction) (*CloudFunction
 }
 
 func (i *Infra) candidateGmailMessages() (*CloudFunction, error) {
-
 	name := "candidate-gmail-messages"
 	sa, err := i.createCloudFunctionServiceAccount(name)
 	if err != nil {
