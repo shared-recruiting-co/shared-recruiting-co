431c76b24f0ab020891145db06f22be65824892c
Devin Stein <devstein@seas.upenn.edu>
2023-02-17 16:11:42 -0800

    chore: generate supabase db migration

--- web/src/lib/supabase/types.ts
+++ web/src/lib/supabase/types.ts
@@ -11,10 +11,10 @@ export interface Database {
 		Functions: {
 			graphql: {
 				Args: {
-					operationName: string;
-					query: string;
-					variables: Json;
-					extensions: Json;
+					operationName?: string;
+					query?: string;
+					variables?: Json;
+					extensions?: Json;
 				};
 				Returns: Json;
 			};
@@ -22,302 +22,305 @@ export interface Database {
 		Enums: {
 			[_ in never]: never;
 		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
 	};
 	public: {
 		Tables: {
 			company: {
 				Row: {
-					company_name: string;
-					website: string;
 					company_id: string;
+					company_name: string;
 					created_at: string;
 					updated_at: string;
+					website: string;
 				};
 				Insert: {
-					company_name: string;
-					website: string;
 					company_id?: string;
+					company_name: string;
 					created_at?: string;
 					updated_at?: string;
+					website: string;
 				};
 				Update: {
-					company_name?: string;
-					website?: string;
 					company_id?: string;
+					company_name?: string;
 					created_at?: string;
 					updated_at?: string;
+					website?: string;
 				};
 			};
 			job: {
 				Row: {
-					title: string;
-					description_url: string;
-					recruiter_id: string;
 					company_id: string;
-					job_id: string;
 					created_at: string;
+					description_url: string;
+					job_id: string;
+					recruiter_id: string;
+					title: string;
 					updated_at: string;
 				};
 				Insert: {
-					title: string;
-					description_url: string;
-					recruiter_id: string;
 					company_id: string;
-					job_id?: string;
 					created_at?: string;
+					description_url: string;
+					job_id?: string;
+					recruiter_id: string;
+					title: string;
 					updated_at?: string;
 				};
 				Update: {
-					title?: string;
-					description_url?: string;
-					recruiter_id?: string;
 					company_id?: string;
-					job_id?: string;
 					created_at?: string;
+					description_url?: string;
+					job_id?: string;
+					recruiter_id?: string;
+					title?: string;
 					updated_at?: string;
 				};
 			};
 			recruiter: {
 				Row: {
-					user_id: string;
+					company_id: string;
+					created_at: string;
 					email: string;
 					first_name: string;
 					last_name: string;
-					company_id: string;
 					responses: Json;
-					created_at: string;
 					updated_at: string;
+					user_id: string;
 				};
 				Insert: {
-					user_id: string;
+					company_id: string;
+					created_at?: string;
 					email: string;
 					first_name: string;
 					last_name: string;
-					company_id: string;
 					responses?: Json;
-					created_at?: string;
 					updated_at?: string;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
+					company_id?: string;
+					created_at?: string;
 					email?: string;
 					first_name?: string;
 					last_name?: string;
-					company_id?: string;
 					responses?: Json;
-					created_at?: string;
 					updated_at?: string;
+					user_id?: string;
 				};
 			};
 			user_email_job: {
 				Row: {
-					user_id: string;
-					user_email: string;
+					company: string;
+					created_at: string;
+					data: Json;
 					email_thread_id: string;
 					emailed_at: string;
-					company: string;
-					job_title: string;
 					job_id: string;
-					data: Json;
-					created_at: string;
+					job_title: string;
 					updated_at: string;
+					user_email: string;
+					user_id: string;
 				};
 				Insert: {
-					user_id: string;
-					user_email: string;
+					company: string;
+					created_at?: string;
+					data?: Json;
 					email_thread_id: string;
 					emailed_at: string;
-					company: string;
-					job_title: string;
 					job_id?: string;
-					data?: Json;
-					created_at?: string;
+					job_title: string;
 					updated_at?: string;
+					user_email: string;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
-					user_email?: string;
+					company?: string;
+					created_at?: string;
+					data?: Json;
 					email_thread_id?: string;
 					emailed_at?: string;
-					company?: string;
-					job_title?: string;
 					job_id?: string;
-					data?: Json;
-					created_at?: string;
+					job_title?: string;
 					updated_at?: string;
+					user_email?: string;
+					user_id?: string;
 				};
 			};
 			user_email_stat: {
 				Row: {
-					user_id: string;
+					created_at: string;
 					email: string;
 					stat_id: string;
 					stat_value: number;
-					created_at: string;
 					updated_at: string;
+					user_id: string;
 				};
 				Insert: {
-					user_id: string;
+					created_at?: string;
 					email: string;
 					stat_id: string;
 					stat_value?: number;
-					created_at?: string;
 					updated_at?: string;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
+					created_at?: string;
 					email?: string;
 					stat_id?: string;
 					stat_value?: number;
-					created_at?: string;
 					updated_at?: string;
+					user_id?: string;
 				};
 			};
 			user_email_sync_history: {
 				Row: {
-					email: string | null;
-					inbox_type: Database['public']['Enums']['inbox_type'];
-					user_id: string;
-					history_id: number;
 					created_at: string;
-					updated_at: string;
+					email: string;
+					history_id: number;
+					inbox_type: Database['public']['Enums']['inbox_type'];
 					synced_at: string;
+					updated_at: string;
+					user_id: string;
 				};
 				Insert: {
-					email?: string | null;
-					inbox_type?: Database['public']['Enums']['inbox_type'];
-					user_id: string;
-					history_id: number;
 					created_at?: string;
-					updated_at?: string;
+					email: string;
+					history_id: number;
+					inbox_type: Database['public']['Enums']['inbox_type'];
 					synced_at?: string;
+					updated_at?: string;
+					user_id: string;
 				};
 				Update: {
-					email?: string | null;
-					inbox_type?: Database['public']['Enums']['inbox_type'];
-					user_id?: string;
-					history_id?: number;
 					created_at?: string;
-					updated_at?: string;
+					email?: string;
+					history_id?: number;
+					inbox_type?: Database['public']['Enums']['inbox_type'];
 					synced_at?: string;
+					updated_at?: string;
+					user_id?: string;
 				};
 			};
 			user_oauth_token: {
 				Row: {
-					user_id: string;
-					provider: string;
 					created_at: string;
-					updated_at: string;
-					token: Json;
 					is_valid: boolean;
+					provider: string;
+					token: Json;
+					updated_at: string;
+					user_id: string;
 				};
 				Insert: {
-					user_id: string;
-					provider: string;
 					created_at?: string;
-					updated_at?: string;
-					token: Json;
 					is_valid?: boolean;
+					provider: string;
+					token: Json;
+					updated_at?: string;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
-					provider?: string;
 					created_at?: string;
-					updated_at?: string;
-					token?: Json;
 					is_valid?: boolean;
+					provider?: string;
+					token?: Json;
+					updated_at?: string;
+					user_id?: string;
 				};
 			};
 			user_profile: {
 				Row: {
-					user_id: string;
-					email: string;
-					first_name: string;
-					last_name: string;
-					updated_at: string;
-					created_at: string;
 					auto_archive: boolean;
 					auto_contribute: boolean;
-					is_active: boolean;
-				};
-				Insert: {
-					user_id: string;
+					created_at: string;
 					email: string;
 					first_name: string;
+					is_active: boolean;
+					last_name: string;
+					updated_at: string;
+					user_id: string;
+				};
+				Insert: {
+					auto_archive?: boolean;
+					auto_contribute?: boolean;
+					created_at?: string;
+					email: string;
+					first_name: string;
+					is_active?: boolean;
 					last_name: string;
 					updated_at?: string;
-					created_at?: string;
-					auto_archive?: boolean;
-					auto_contribute?: boolean;
-					is_active?: boolean;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
-					email?: string;
-					first_name?: string;
-					last_name?: string;
-					updated_at?: string;
-					created_at?: string;
 					auto_archive?: boolean;
 					auto_contribute?: boolean;
+					created_at?: string;
+					email?: string;
+					first_name?: string;
 					is_active?: boolean;
+					last_name?: string;
+					updated_at?: string;
+					user_id?: string;
 				};
 			};
 			waitlist: {
 				Row: {
-					user_id: string;
+					can_create_account: boolean;
+					created_at: string;
 					email: string;
 					first_name: string;
 					last_name: string;
 					linkedin_url: string;
 					responses: Json;
-					can_create_account: boolean;
-					created_at: string;
 					updated_at: string;
+					user_id: string;
 				};
 				Insert: {
-					user_id: string;
+					can_create_account?: boolean;
+					created_at?: string;
 					email: string;
 					first_name: string;
 					last_name: string;
 					linkedin_url: string;
 					responses?: Json;
-					can_create_account?: boolean;
-					created_at?: string;
 					updated_at?: string;
+					user_id: string;
 				};
 				Update: {
-					user_id?: string;
+					can_create_account?: boolean;
+					created_at?: string;
 					email?: string;
 					first_name?: string;
 					last_name?: string;
 					linkedin_url?: string;
 					responses?: Json;
-					can_create_account?: boolean;
-					created_at?: string;
 					updated_at?: string;
+					user_id?: string;
 				};
 			};
 		};
 		Views: {
 			candidate_oauth_token: {
 				Row: {
-					user_id: string | null;
+					created_at: string | null;
+					is_valid: boolean | null;
 					provider: string | null;
 					token: Json | null;
-					created_at: string | null;
 					updated_at: string | null;
-					is_valid: boolean | null;
+					user_id: string | null;
 				};
 			};
 			recruiter_oauth_token: {
 				Row: {
-					user_id: string | null;
+					created_at: string | null;
+					is_valid: boolean | null;
 					provider: string | null;
 					token: Json | null;
-					created_at: string | null;
 					updated_at: string | null;
-					is_valid: boolean | null;
+					user_id: string | null;
 				};
 			};
 		};
@@ -335,88 +338,91 @@ export interface Database {
 		Enums: {
 			inbox_type: 'candidate' | 'recruiter';
 		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
 	};
 	storage: {
 		Tables: {
 			buckets: {
 				Row: {
+					created_at: string | null;
 					id: string;
 					name: string;
 					owner: string | null;
-					created_at: string | null;
-					updated_at: string | null;
 					public: boolean | null;
+					updated_at: string | null;
 				};
 				Insert: {
+					created_at?: string | null;
 					id: string;
 					name: string;
 					owner?: string | null;
-					created_at?: string | null;
-					updated_at?: string | null;
 					public?: boolean | null;
+					updated_at?: string | null;
 				};
 				Update: {
+					created_at?: string | null;
 					id?: string;
 					name?: string;
 					owner?: string | null;
-					created_at?: string | null;
-					updated_at?: string | null;
 					public?: boolean | null;
+					updated_at?: string | null;
 				};
 			};
 			migrations: {
 				Row: {
+					executed_at: string | null;
+					hash: string;
 					id: number;
 					name: string;
-					hash: string;
-					executed_at: string | null;
 				};
 				Insert: {
+					executed_at?: string | null;
+					hash: string;
 					id: number;
 					name: string;
-					hash: string;
-					executed_at?: string | null;
 				};
 				Update: {
+					executed_at?: string | null;
+					hash?: string;
 					id?: number;
 					name?: string;
-					hash?: string;
-					executed_at?: string | null;
 				};
 			};
 			objects: {
 				Row: {
 					bucket_id: string | null;
+					created_at: string | null;
+					id: string;
+					last_accessed_at: string | null;
+					metadata: Json | null;
 					name: string | null;
 					owner: string | null;
-					metadata: Json | null;
-					id: string;
-					created_at: string | null;
-					updated_at: string | null;
-					last_accessed_at: string | null;
 					path_tokens: string[] | null;
+					updated_at: string | null;
 				};
 				Insert: {
 					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
 					name?: string | null;
 					owner?: string | null;
-					metadata?: Json | null;
-					id?: string;
-					created_at?: string | null;
-					updated_at?: string | null;
-					last_accessed_at?: string | null;
 					path_tokens?: string[] | null;
+					updated_at?: string | null;
 				};
 				Update: {
 					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
 					name?: string | null;
 					owner?: string | null;
-					metadata?: Json | null;
-					id?: string;
-					created_at?: string | null;
-					updated_at?: string | null;
-					last_accessed_at?: string | null;
 					path_tokens?: string[] | null;
+					updated_at?: string | null;
 				};
 			};
 		};
@@ -425,31 +431,40 @@ export interface Database {
 		};
 		Functions: {
 			extension: {
-				Args: { name: string };
+				Args: {
+					name: string;
+				};
 				Returns: string;
 			};
 			filename: {
-				Args: { name: string };
+				Args: {
+					name: string;
+				};
 				Returns: string;
 			};
 			foldername: {
-				Args: { name: string };
+				Args: {
+					name: string;
+				};
 				Returns: string[];
 			};
 			get_size_by_bucket: {
 				Args: Record<PropertyKey, never>;
-				Returns: { size: number; bucket_id: string }[];
+				Returns: {
+					size: number;
+					bucket_id: string;
+				}[];
 			};
 			search: {
 				Args: {
 					prefix: string;
 					bucketname: string;
-					limits: number;
-					levels: number;
-					offsets: number;
-					search: string;
-					sortcolumn: string;
-					sortorder: string;
+					limits?: number;
+					levels?: number;
+					offsets?: number;
+					search?: string;
+					sortcolumn?: string;
+					sortorder?: string;
 				};
 				Returns: {
 					name: string;
@@ -462,6 +477,9 @@ export interface Database {
 			};
 		};
 		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
 			[_ in never]: never;
 		};
 	};
