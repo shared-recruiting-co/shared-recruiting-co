5d90e121eb1410c1ae698adbce4e9d8e1bd61ef2
Devin Stein <devstein@seas.upenn.edu>
2023-02-17 17:23:29 -0800

    fix: delete unused code and test

--- cloudfunctions/candidate_gmail_push_notifications/email.go
+++ cloudfunctions/candidate_gmail_push_notifications/email.go
@@ -4,7 +4,6 @@ import (
 	"google.golang.org/api/gmail/v1"
 
 	srcmail "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail"
-	srcmessage "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail/message"
 )
 
 const maxResults = 250
@@ -35,18 +34,4 @@ func fetchNewEmailsSinceHistoryID(srv *srcmail.Service, historyID uint64, pageTo
 	}
 
 	return messages, r.NextPageToken, nil
-}
-
-// skipThread if the thread
-// - thread was started by the user or the user has replied
-// - thread has a job label
-func skipThread(messages []*gmail.Message, labelID string) bool {
-	for _, m := range messages {
-		if m.LabelIds != nil {
-			if srcmessage.HasLabel(m, "SENT") || srcmessage.HasLabel(m, labelID) {
-				return true
-			}
-		}
-	}
-	return false
 }
