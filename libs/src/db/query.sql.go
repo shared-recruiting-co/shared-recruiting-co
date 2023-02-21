d073c8fe5264e714a97e09ac58d1bc357c7a0251
Devin Stein <devstein@seas.upenn.edu>
2023-02-20 17:13:47 -0800

    chore: sqlc generate

--- libs/src/db/query.sql.go
+++ libs/src/db/query.sql.go
@@ -28,15 +28,16 @@ func (q *Queries) CountUserEmailJobs(ctx context.Context, userID uuid.UUID) (int
 
 const getRecruiterByEmail = `-- name: GetRecruiterByEmail :one
 select 
-    user_id,
-    email,
-    first_name,
-    last_name,
-    company_id,
-    created_at,
-    updated_at
+    recruiter.user_id,
+    recruiter.email,
+    recruiter.first_name,
+    recruiter.last_name,
+    recruiter.company_id,
+    recruiter.created_at,
+    recruiter.updated_at
 from recruiter
-where email = $1
+inner join public.user_oauth_token using (user_id)
+where recruiter.email = $1 OR user_oauth_token.email = $1
 `
 
 type GetRecruiterByEmailRow struct {
@@ -168,17 +169,18 @@ func (q *Queries) GetUserOAuthToken(ctx context.Context, arg GetUserOAuthTokenPa
 
 const getUserProfileByEmail = `-- name: GetUserProfileByEmail :one
 select
-    user_id,
-    email,
-    first_name,
-    last_name,
-    is_active,
-    auto_archive,
-    auto_contribute,
-    created_at,
-    updated_at
+    user_profile.user_id,
+    user_profile.email,
+    user_profile.first_name,
+    user_profile.last_name,
+    user_profile.is_active,
+    user_profile.auto_archive,
+    user_profile.auto_contribute,
+    user_profile.created_at,
+    user_profile.updated_at
 from public.user_profile
-where email = $1
+inner join public.user_oauth_token using (user_id)
+where user_profile.email = $1 OR user_oauth_token.email = $1
 `
 
 func (q *Queries) GetUserProfileByEmail(ctx context.Context, email string) (UserProfile, error) {
