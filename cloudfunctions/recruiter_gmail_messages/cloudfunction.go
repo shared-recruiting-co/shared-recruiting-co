33885eb8659f9e96d79784f87bbd958bb286b288
Devin Stein <devstein@seas.upenn.edu>
2023-02-21 12:42:31 -0800

    feat: handle outbound recruiter messages

--- cloudfunctions/recruiter_gmail_messages/cloudfunction.go
+++ cloudfunctions/recruiter_gmail_messages/cloudfunction.go
@@ -2,20 +2,31 @@ package cloudfunctions
 
 import (
 	"context"
+	"database/sql"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"html"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/getsentry/sentry-go"
+	"github.com/google/uuid"
+	"github.com/jaytaylor/html2text"
+
+	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/idtoken"
 
 	"github.com/shared-recruiting-co/shared-recruiting-co/libs/src/db"
 	srcmail "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail"
+	srclabel "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail/label"
+	srcmessage "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail/message"
 	"github.com/shared-recruiting-co/shared-recruiting-co/libs/src/ml"
 	"github.com/shared-recruiting-co/shared-recruiting-co/libs/src/pubsub/schema"
 )
@@ -46,6 +57,7 @@ type CloudFunction struct {
 	ctx      context.Context
 	queries  db.Querier
 	srv      *srcmail.Service
+	labels   *srclabel.RecruiterLabels
 	model    ml.Service
 	user     db.GetRecruiterByEmailRow
 	settings schema.EmailMessagesSettings
@@ -90,6 +102,12 @@ func NewCloudFunction(ctx context.Context, payload schema.EmailMessages) (*Cloud
 		return nil, fmt.Errorf("error creating gmail service: %w", err)
 	}
 
+	// Get the SRC labels
+	labels, err := srv.GetOrCreateRecruiterLabels()
+	if err != nil {
+		return nil, fmt.Errorf("error getting recruiter labels: %w", err)
+	}
+
 	// Create recruiting detector client
 	classifierBaseURL := os.Getenv("ML_SERVICE_URL")
 	idTokenSource, err := idtoken.NewTokenSource(ctx, classifierBaseURL)
@@ -111,6 +129,7 @@ func NewCloudFunction(ctx context.Context, payload schema.EmailMessages) (*Cloud
 		ctx:      ctx,
 		queries:  queries,
 		srv:      srv,
+		labels:   labels,
 		model:    model,
 		user:     user,
 		settings: payload.Settings,
@@ -155,10 +174,265 @@ func handler(ctx context.Context, e event.Event) error {
 		return nil
 	}
 
-	_, err = NewCloudFunction(ctx, payload)
+	cf, err := NewCloudFunction(ctx, payload)
 	if err != nil {
 		return handleError("error creating cloud function", err)
 	}
 
+	for _, m := range payload.Messages {
+		err = cf.processMessage(m)
+		if err != nil {
+			// for now abort on first error
+			return handleError("error creating cloud function", err)
+		}
+	}
+
 	return nil
+}
+
+// processMessage processes a single message
+//
+// Logic
+// 1. Given message, get thread
+// 2. Check if thread is already labeled if so skip
+// 3. Get the first message in the thread
+//
+// 4. Check if the first message in the thread exists in the database
+// 5. If it does, skip
+// 6. If it doesn't check for a matching template
+// 7. If there is a matching template, save the message to the database and label
+// 8. If not, check if the message is a recruiting email (model.Classify)
+// 9. If not, skip
+// 10. If it is, save the message as a template, save the message to the database and label
+func (cf *CloudFunction) processMessage(id string) error {
+	// 1. Get Message
+	msg, err := cf.srv.GetMessage(id)
+	if err != nil {
+		return fmt.Errorf("error getting message: %w", err)
+	}
+
+	// 2. Get Thread
+	thread, err := cf.srv.GetThread(msg.ThreadId, "minimal")
+	if err != nil {
+		return fmt.Errorf("error getting thread: %w", err)
+	}
+
+	// 3. Check if thread is already labeled or if the earliest message is not from the user
+	if !isThreadStartedByUser(thread.Messages) || isThreadLabeled(thread.Messages, cf.labels.RecruitingOutbound.Id) {
+		log.Printf("thread %s already labeled or not started by user, skipping", thread.Id)
+		return nil
+	}
+
+	// 4. Get the first message in the thread
+	// Two options:
+	// a. There are multiple messages in the thread
+	// b. There is only one message in the thread. In this case, we search for previous emails to the recipient
+	//    and use the first one as the first message in the thread
+	var firstMsg *gmail.Message
+	if len(thread.Messages) > 1 {
+		srcmessage.SortByDate(thread.Messages)
+		firstMsg = thread.Messages[0]
+	} else {
+		// TODO: search for previous emails to the recipient within 3 months
+		firstMsg = msg
+	}
+
+	// 5. Check if the first message in the thread exists in the database
+	existing, err := cf.queries.GetRecruiterOutboundMessage(cf.ctx, db.GetRecruiterOutboundMessageParams{
+		RecruiterID: cf.user.UserID,
+		MessageID:   firstMsg.Id,
+	})
+
+	if err != nil && err != sql.ErrNoRows {
+		return fmt.Errorf("error checking if first message exists in the database: %w", err)
+	}
+	if existing.MessageID != "" {
+		log.Printf("first message %s already exists in the database, skipping", existing.MessageID)
+		return nil
+	}
+
+	if firstMsg.Id != msg.Id {
+		firstMsg, err = cf.srv.GetMessage(firstMsg.Id)
+		if err != nil {
+			return fmt.Errorf("error getting first message: %w", err)
+		}
+	}
+	// extract first message subject and body text
+	subject := srcmessage.Subject(firstMsg)
+	body := srcmessage.Body(firstMsg)
+	formattedBody := cleanBody(body)
+	input := fmt.Sprintf("%s %s", subject, formattedBody)
+
+	// 6. If it doesn't check for a matching template
+	templates, err := cf.queries.ListSimilarRecruiterOutboundTemplates(cf.ctx, db.ListSimilarRecruiterOutboundTemplatesParams{
+		UserID: cf.user.UserID,
+		Input:  input,
+	})
+	if err != nil {
+		return fmt.Errorf("error getting similar templates: %w", err)
+	}
+
+	// matching template
+	// 7. If there is a matching template, save the message to the database and label
+	if len(templates) > 0 {
+		// for now, print all matching templates and match to the first one
+		log.Printf("found %d matching templates", len(templates))
+		for _, t := range templates {
+			log.Printf("template: %s / similarity %0.2f", t.TemplateID, t.Similarity)
+		}
+
+		// match to the first template
+		template := templates[0]
+
+		err = cf.queries.InsertRecruiterOutboundMessage(cf.ctx, db.InsertRecruiterOutboundMessageParams{
+			RecruiterID: cf.user.UserID,
+			MessageID:   firstMsg.Id,
+			TemplateID: uuid.NullUUID{
+				UUID:  template.TemplateID,
+				Valid: true,
+			},
+			InternalMessageID: srcmessage.HostMessageID(firstMsg),
+			FromEmail:         srcmessage.SenderEmail(firstMsg),
+			ToEmail:           srcmessage.RecipientEmail(firstMsg),
+			SentAt:            srcmessage.CreatedAt(firstMsg),
+		})
+		if err != nil {
+			return fmt.Errorf("error inserting message: %w", err)
+		}
+
+		// label thread
+		_, err = srcmail.ExecuteWithRetries(func() (interface{}, error) {
+			return cf.srv.Users.Threads.Modify(cf.srv.UserID, thread.Id, &gmail.ModifyThreadRequest{
+				// Add job opportunity label and parent folder labels
+				AddLabelIds: []string{cf.labels.SRC.Id, cf.labels.Recruiting.Id, cf.labels.RecruitingOutbound.Id},
+			}).Do()
+		})
+		if err != nil {
+			return fmt.Errorf("error labeling thread: %w", err)
+		}
+
+		// done
+		return nil
+	}
+	log.Printf("no matching template found for message %s", msg.Id)
+
+	// 8. If not, check if the message is a recruiting email (model.Classify)
+	classification, err := cf.model.Classify(&ml.EmailInput{
+		From:    srcmessage.Sender(firstMsg),
+		Subject: subject,
+		// Use raw body
+		Body: body,
+	})
+	if err != nil {
+		return fmt.Errorf("error classifying email: %w", err)
+	}
+	// 9. If not, skip
+	if !classification.Result {
+		log.Printf("first message %s is not a recruiting email, skipping", firstMsg.Id)
+		return nil
+	}
+
+	// 10. If it is, save the message as a template, save the message to the database and label
+	metadata := map[string]interface{}{
+		"MessageID": firstMsg.Id,
+		"SentAt":    srcmessage.CreatedAt(firstMsg),
+	}
+	metadataJSON, err := json.Marshal(metadata)
+	if err != nil {
+		return fmt.Errorf("error marshaling template metadata: %w", err)
+	}
+	template, err := cf.queries.InsertRecruiterOutboundTemplate(cf.ctx, db.InsertRecruiterOutboundTemplateParams{
+		RecruiterID: cf.user.UserID,
+		Subject:     subject,
+		Body:        formattedBody,
+		Metadata:    metadataJSON,
+	})
+	if err != nil {
+		return fmt.Errorf("error inserting template: %w", err)
+	}
+
+	err = cf.queries.InsertRecruiterOutboundMessage(cf.ctx, db.InsertRecruiterOutboundMessageParams{
+		RecruiterID: cf.user.UserID,
+		MessageID:   firstMsg.Id,
+		TemplateID: uuid.NullUUID{
+			UUID:  template.TemplateID,
+			Valid: true,
+		},
+		InternalMessageID: srcmessage.HostMessageID(firstMsg),
+		FromEmail:         srcmessage.SenderEmail(firstMsg),
+		ToEmail:           srcmessage.RecipientEmail(firstMsg),
+		SentAt:            srcmessage.CreatedAt(firstMsg),
+	})
+
+	// label thread
+	_, err = srcmail.ExecuteWithRetries(func() (interface{}, error) {
+		return cf.srv.Users.Threads.Modify(cf.srv.UserID, thread.Id, &gmail.ModifyThreadRequest{
+			// Add job opportunity label and parent folder labels
+			AddLabelIds: []string{cf.labels.SRC.Id, cf.labels.Recruiting.Id, cf.labels.RecruitingOutbound.Id},
+		}).Do()
+	})
+	if err != nil {
+		return fmt.Errorf("error labeling thread: %w", err)
+	}
+
+	return nil
+}
+
+// isThreadLabeled if the messages already labeled with SRC label
+func isThreadLabeled(messages []*gmail.Message, labelID string) bool {
+	if len(messages) == 0 {
+		return true
+	}
+
+	// for each message in the thread, check if it has the @src label
+	for _, m := range messages {
+		if srcmessage.HasLabel(m, labelID) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isThreadStartedByUser(messages []*gmail.Message) bool {
+	if len(messages) == 0 {
+		return false
+	}
+	// get the first message in the thread
+	srcmessage.SortByDate(messages)
+	firstMsg := messages[0]
+	return srcmessage.IsSent(firstMsg)
+}
+
+func cleanBody(body string) string {
+	// convert to text if it's HTML
+	mime := http.DetectContentType([]byte(body))
+	// mime will be "text/html; charset=utf-8"
+	if strings.HasPrefix(mime, "text/html") {
+		text, err := html2text.FromString(body,
+			html2text.Options{
+				PrettyTables: false,
+				OmitLinks:    true,
+				TextOnly:     true,
+			})
+		if err != nil {
+			// log
+			log.Printf("error converting html to text: %v", err)
+		} else {
+			body = text
+		}
+	}
+	// remove html tags
+	body = html.UnescapeString(body)
+
+	// remove extra spaces
+	body = strings.ReplaceAll(body, "\n", " ")
+	body = strings.ReplaceAll(body, "\t", " ")
+	body = strings.ReplaceAll(body, "  ", " ")
+
+	// strip links because they are often customized per recipient
+	re := regexp.MustCompile(`https?://\S+`)
+	body = re.ReplaceAllString(body, "")
+
+	return body
 }
