3eefb92adc38b49876a20b2f253f32d83d897e8c
Devin Stein <devstein@seas.upenn.edu>
2023-02-21 06:53:23 -0800

    feat: add unique constraint on (email/provider)

--- web/supabase/migrations/20230221145139_unique_email_provider.sql
+++ web/supabase/migrations/20230221145139_unique_email_provider.sql
@@ -1,5 +1,3 @@
-CREATE UNIQUE INDEX unique_email_provider ON public.user_oauth_token USING btree (email, provider);
+create unique index unique_email_provider on public.user_oauth_token using btree (email, provider);
 
-alter table "public"."user_oauth_token" add constraint "unique_email_provider" UNIQUE using index "unique_email_provider";
-
-
+alter table "public"."user_oauth_token" add constraint "unique_email_provider" unique using index "unique_email_provider";
