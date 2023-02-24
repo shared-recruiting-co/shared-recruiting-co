c7d1862327b997b04a3fef688204c8a9ab5fc575
Devin Stein <devstein@seas.upenn.edu>
2023-02-24 14:02:52 -0800

    test: add another canary test

--- web/supabase/tests/database/canary.test.sql
+++ web/supabase/tests/database/canary.test.sql
@@ -4,14 +4,22 @@
 -- This should always pass. If it doesn't the test infra is broken.
 --------------
 begin;
-select plan( 1 );
+select plan( 2 );
 
+-- is pgtap installed
 select results_eq(
     'select * from (values (1))  as t',
     $$VALUES (1)$$,
     'canary'
 );
 
+-- is supabase database setup
+SELECT has_column(
+    'auth',
+    'users',
+    'id',
+    'id should exist'
+);
 
 select * from finish();
 rollback;
