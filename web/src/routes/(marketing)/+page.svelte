a5de225fefdd56a6a4da947cb9c6438bf836f73b
Devin Stein <devstein@seas.upenn.edu>
2023-03-07 17:57:48 -0800

    style: update landing page copy

--- web/src/routes/(marketing)/+page.svelte
+++ web/src/routes/(marketing)/+page.svelte
@@ -16,9 +16,7 @@
 		<span class="font-medium text-blue-600">So are we.</span>
 	</h1>
 	<p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-		The Shared Recruiting Co. (SRC) is an AI recruiting assistant that lives in your inbox. SRC
-		keeps your inbox clean when you aren’t looking for jobs and supercharges your job search once
-		you are.
+		The Shared Recruiting Co. (SRC) is an open-source, candidate-centric recruiting platform. SRC makes recruiting more effecient and less painful by promoting transparent, opt-in communication between candidates and companies.
 	</p>
 	<div class="mt-10 flex justify-center gap-x-6">
 		<a
