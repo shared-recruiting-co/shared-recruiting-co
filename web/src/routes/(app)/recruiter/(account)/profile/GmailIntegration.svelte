0d4d4a77f0faf3e58cb0f92a06d6ccca5591f6e3
Devin Stein <devstein@seas.upenn.edu>
2023-03-21 08:17:44 -0700

    fix: move color to class

--- web/src/routes/(app)/recruiter/(account)/profile/GmailIntegration.svelte
+++ web/src/routes/(app)/recruiter/(account)/profile/GmailIntegration.svelte
@@ -121,12 +121,12 @@
 						Open: "-rotate-180", Closed: "rotate-0"
 					-->
 				<svg
-					class="h-6 w-6 transform"
+					class="h-6 w-6 transform text-slate-600"
 					class:-rotate-180={isOpen}
 					class:rotate-0={!isOpen}
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
-					viewBox="0 0 24 24 text-slate-600"
+					viewBox="0 0 24 24"
 					stroke-width="2"
 					stroke="currentColor"
 					aria-hidden="true"
