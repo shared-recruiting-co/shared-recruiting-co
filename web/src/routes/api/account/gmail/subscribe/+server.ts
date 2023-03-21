1789fb5a807a868d031c026e1eb68bb52ca1943d
Devin Stein <devstein@seas.upenn.edu>
2023-03-20 18:56:47 -0700

    feat: watch recruiter gmail if account exists

--- web/src/routes/api/account/gmail/subscribe/+server.ts
+++ web/src/routes/api/account/gmail/subscribe/+server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-import { CANDIDATE_GMAIL_PUBSUB_TOPIC } from '$env/static/private';
+import { CANDIDATE_GMAIL_PUBSUB_TOPIC, RECRUITER_GMAIL_PUBSUB_TOPIC } from '$env/static/private';
 
 import { watch } from '$lib/server/google/gmail';
 import type { WatchRequest } from '$lib/server/google/gmail';
@@ -11,6 +11,12 @@ const candidateWatchRequest: WatchRequest = {
 	labelIds: ['UNREAD'],
 	labelFilterAction: 'include',
 	topicName: CANDIDATE_GMAIL_PUBSUB_TOPIC
+};
+
+const recruiterWatchRequest: WatchRequest = {
+	labelIds: ['SENT'],
+	labelFilterAction: 'include',
+	topicName: RECRUITER_GMAIL_PUBSUB_TOPIC
 };
 
 export const POST: RequestHandler = async ({ request, locals: { getSession, supabase } }) => {
@@ -33,18 +39,42 @@ export const POST: RequestHandler = async ({ request, locals: { getSession, supa
 
 		throw error(500, 'unexpected error occurred');
 	}
-	// watch for new emails
-	const watchResponse = await watch(accessToken, candidateWatchRequest);
 
-	if (watchResponse.status !== 200) throw error(500, 'Failed to subscribe to gmail notifications');
+	// check if user is a candidate or recruiter
+	const { data: candidate } = await supabase.from('user_profile').select('user_id').maybeSingle();
+	if (candidate) {
+		// watch for new emails
+		const watchResponse = await watch(accessToken, candidateWatchRequest);
 
-	// "activate" the email in db
-	const { error: updateError } = await supabase
-		.from('user_profile')
-		.update({ is_active: true })
-		.eq('user_id', session?.user.id);
+		if (watchResponse.status !== 200)
+			throw error(500, 'Failed to subscribe to gmail notifications');
 
-	if (updateError) throw error(500, 'failed to saved changes to database');
+		// "activate" the email in db
+		const { error: updateError } = await supabase
+			.from('user_profile')
+			.update({ is_active: true })
+			.eq('user_id', session?.user.id);
+
+		if (updateError) throw error(500, 'failed to saved changes to database');
+	}
+
+	const { data: recruiter } = await supabase.from('recruiter').select('user_id').maybeSingle();
+	if (recruiter) {
+		// watch for new emails
+		const watchResponse = await watch(accessToken, recruiterWatchRequest);
+
+		if (watchResponse.status !== 200)
+			throw error(500, 'Failed to subscribe to gmail notifications');
+
+		// TODO
+		// "activate" the email in db
+		// const { error: updateError } = await supabase
+		// .from('user_profile')
+		// .update({ is_active: true })
+		// .eq('user_id', session?.user.id);
+		//
+		// if (updateError) throw error(500, 'failed to saved changes to database');
+	}
 
 	return new Response('success');
 };
