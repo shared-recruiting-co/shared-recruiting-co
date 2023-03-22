e71be46803891dae704eaeb68dff66fb4095f8da
Devin Stein <devstein@seas.upenn.edu>
2023-03-21 18:27:49 -0700

    feat: add outbound to sidenav

--- web/src/routes/(app)/recruiter/(account)/Sidenav.svelte
+++ web/src/routes/(app)/recruiter/(account)/Sidenav.svelte
@@ -42,6 +42,13 @@
 			`
 		},
 		{
+			name: 'Outbound',
+			href: '/recruiter/outbound',
+			icon: `
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+			`
+		},
+		{
 			name: 'Settings',
 			href: '/recruiter/settings',
 			icon: `
