0b4acb319fa367688e2ae80e00270101c8ce6cf7
Devin Stein <devstein@seas.upenn.edu>
2023-03-21 18:28:22 -0700

    feat: initial outbound template / sequences page

---
+++ web/src/routes/(app)/recruiter/(account)/outbound/+page.ts
@@ -0,0 +1,74 @@
+import type { PageLoad } from './$types';
+
+import type { Database } from '$lib/supabase/types';
+
+const PAGE_SIZE = 10;
+
+type Pagination = {
+	page: number;
+	perPage: number;
+	numPages: number;
+	numResults: number;
+	hasNext: boolean;
+	hasPrev: boolean;
+};
+
+type Data = {
+	pagination: Pagination;
+	sequences: Database['public']['Tables']['recruiter_outbound_template']['Row'][];
+	jobs: {
+		job_id: string;
+		title: string;
+	}[];
+};
+
+export const load: PageLoad<Data> = async ({ url, parent }) => {
+	const { supabase } = await parent();
+
+	// assume parent is checking for session
+	const page = parseInt(url.searchParams.get('page') || '1') || 1;
+	const start = (page - 1) * PAGE_SIZE;
+
+	const stop = start + PAGE_SIZE;
+
+	// Get outbound recruiter template
+	const { data: outboundTemplates } = await supabase
+		.from('recruiter_outbound_template')
+		.select('*,job(*)')
+		.order('created_at', { ascending: false })
+		.range(start, stop);
+
+	let { count, error: countError } = await supabase
+		.from('recruiter_outbound_template')
+		.select('*', {
+			head: true,
+			count: 'exact'
+		});
+
+	// TODO: decide on error handling
+	if (countError) {
+		console.error(countError);
+	}
+	if (count === null) {
+		count = 0;
+	}
+
+	// TODO: Get count of messages (candidates) per template
+
+	// fetch possible jobs
+	// TODO: Consider doing this client-side in the combo box
+	const { data: jobs } = await supabase.from('job').select('job_id,title');
+
+	return {
+		sequences: outboundTemplates,
+		jobs,
+		pagination: {
+			page,
+			perPage: PAGE_SIZE,
+			numPages: Math.ceil(count / PAGE_SIZE),
+			numResults: count,
+			hasPrev: page > 1,
+			hasNext: page < Math.ceil(count / PAGE_SIZE)
+		}
+	};
+};
