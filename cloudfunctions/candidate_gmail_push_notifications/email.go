94012bd181b1b821466a22ed3fc7af9cd532d1c7
Devin Stein <devstein@seas.upenn.edu>
2023-04-02 17:19:03 -0700

    feat: fetch email label changes from history

--- cloudfunctions/candidate_gmail_push_notifications/email.go
+++ cloudfunctions/candidate_gmail_push_notifications/email.go
@@ -4,18 +4,29 @@ import (
 	"google.golang.org/api/gmail/v1"
 
 	srcmail "github.com/shared-recruiting-co/shared-recruiting-co/libs/src/mail/gmail"
+	"github.com/shared-recruiting-co/shared-recruiting-co/libs/src/pubsub/schema"
 )
 
 const maxResults = 250
 const historyTypeMessageAdded = "messageAdded"
 
-func fetchNewEmailsSinceHistoryID(srv *srcmail.Service, historyID uint64, pageToken string) ([]*gmail.Message, string, error) {
+var (
+	// historyTypes is a list of all the history types that we want to fetch.
+	// We don't care about deleted messages.
+	// See https://developers.google.com/gmail/api/v1/reference/users/history/list
+	historyTypes = []string{
+		"messageAdded",
+		"labelAdded",
+		"labelRemoved",
+	}
+)
+
+func fetchChangesSinceHistoryID(srv *srcmail.Service, historyID uint64, pageToken string) ([]*gmail.History, string, error) {
 	r, err := srcmail.ExecuteWithRetries(func() (*gmail.ListHistoryResponse, error) {
 		return srv.Users.History.
 			List(srv.UserID).
-			LabelId("UNREAD").
 			StartHistoryId(historyID).
-			HistoryTypes(historyTypeMessageAdded).
+			HistoryTypes(historyTypes...).
 			PageToken(pageToken).
 			MaxResults(maxResults).
 			Do()
@@ -33,5 +44,39 @@ func fetchNewEmailsSinceHistoryID(srv *srcmail.Service, historyID uint64, pageTo
 		}
 	}
 
-	return messages, r.NextPageToken, nil
+	return r.History, r.NextPageToken, nil
+}
+
+func historyToAddedMessages(histories []*gmail.History) []*gmail.Message {
+	messages := []*gmail.Message{}
+	for _, h := range histories {
+		for _, m := range h.MessagesAdded {
+			messages = append(messages, m.Message)
+		}
+	}
+	return messages
+}
+
+func (cf *CloudFunction) historyToEmailLabelChanges(histories []*gmail.History) *schema.EmailLabelChanges {
+	changes := []schema.EmailLabelChange{}
+	for _, h := range histories {
+		for _, m := range h.LabelsAdded {
+			changes = append(changes, schema.EmailLabelChange{
+				MessageID:  m.Message.Id,
+				LabelIDs:   m.LabelIds,
+				ChangeType: schema.EmailLabelChangeTypeAdded,
+			})
+		}
+		for _, m := range h.LabelsRemoved {
+			changes = append(changes, schema.EmailLabelChange{
+				MessageID:  m.Message.Id,
+				LabelIDs:   m.LabelIds,
+				ChangeType: schema.EmailLabelChangeTypeRemoved,
+			})
+		}
+	}
+	return &schema.EmailLabelChanges{
+		Email:   cf.payload.Email,
+		Changes: &changes,
+	}
 }
