91142df2604de598af5d2d719400d8bad3df5223
Mark Steinbrick <marksteinbrick95+github@googlemail.com>
2023-04-05 18:09:33 -0700

    move remove job if condition inside <td>

--- web/src/routes/(app)/account/jobs/+page.svelte
+++ web/src/routes/(app)/account/jobs/+page.svelte
@@ -228,8 +228,8 @@
 									</div>
 								</div>
 							</td>
-							{#if !job.is_verified}
 							<td class="align-top px-3 py-4 flex justify-end">
+								{#if !job.is_verified}
 								<button 
 									class="hover:text-red-500"
 									title="Remove this job"
@@ -245,8 +245,8 @@
 											<path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 										</svg> 
 								</button>
+								{/if}
 							</td>
-							{/if}
 						</tr>
 					{/each}
 				</tbody>
