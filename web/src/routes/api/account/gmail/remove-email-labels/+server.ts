0f215f80a66e90627578d82c1e9738e1ebec7ede
Devin Stein <devstein@seas.upenn.edu>
2023-04-10 17:49:57 -0700

    chore: run prettier, remove unused imports

--- web/src/routes/api/account/gmail/remove-email-labels/+server.ts
+++ web/src/routes/api/account/gmail/remove-email-labels/+server.ts
@@ -1,14 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { getRefreshedGoogleAccessToken } from '$lib/supabase/client.server';
-import { email } from '$lib/components/ConnectGoogleAccountButton.svelte';
-import {
-  getSrcLabelIds,
-  getThreadLabels,
-  removeLabelsFromThread,
-  isValidThread
-} from '$lib/server/google/gmail';
-
+import { getSrcLabelIds, removeLabelsFromThread, isValidThread } from '$lib/server/google/gmail';
 
 /**
  * Main handler function for this POST request route
@@ -20,43 +13,42 @@ import {
  * @throws Throws an error if the user is not authorized or if an unexpected error occurs.
  */
 export const POST: RequestHandler = async ({ request, locals: { getSession, supabase } }) => {
-  
-  // Get the current user's session
-  const session = await getSession();
-  if (!session) throw error(401, 'unauthorized');
+	// Get the current user's session
+	const session = await getSession();
+	if (!session) throw error(401, 'unauthorized');
 
-  // retrieve the necessary information from the request 
-  const { email, email_thread_id: threadId } = await request.json();
+	// retrieve the necessary information from the request
+	const { email, email_thread_id: threadId } = await request.json();
 
-  if (!email || !threadId) {
-    throw error(400, 'Could not find both email and email_thread_id from request json');
-  }
+	if (!email || !threadId) {
+		throw error(400, 'Could not find both email and email_thread_id from request json');
+	}
 
-  // Get a refreshed Google access token for the current user
-  let accessToken = '';
-  try {
-    accessToken = await getRefreshedGoogleAccessToken(supabase, email);
-  } catch (err) {
-    throw error(500, err instanceof Error ? err : 'Could not retrieve access token');
-  }
+	// Get a refreshed Google access token for the current user
+	let accessToken = '';
+	try {
+		accessToken = await getRefreshedGoogleAccessToken(supabase, email);
+	} catch (err) {
+		throw error(500, err instanceof Error ? err : 'Could not retrieve access token');
+	}
 
-  // check the thread actually exists in Gmail before attempting the update
-  const validThread = await isValidThread(accessToken, threadId)
-  if (!validThread) {
-    throw error(404, 'No corresponding Gmail thread');
-  }
+	// check the thread actually exists in Gmail before attempting the update
+	const validThread = await isValidThread(accessToken, threadId);
+	if (!validThread) {
+		throw error(404, 'No corresponding Gmail thread');
+	}
 
-  // Get the label IDs for all "@SRC" labels
-  const srcLabelIds = await getSrcLabelIds(accessToken);
+	// Get the label IDs for all "@SRC" labels
+	const srcLabelIds = await getSrcLabelIds(accessToken);
 
-  // Remove all "@SRC" labels from the specified Gmail thread
-  const success = await removeLabelsFromThread(accessToken, threadId, srcLabelIds);
+	// Remove all "@SRC" labels from the specified Gmail thread
+	const success = await removeLabelsFromThread(accessToken, threadId, srcLabelIds);
 
-  // handle the return value of removeLabelsFromThread 
-  if (!success) {
-    throw error(500, 'Failed to remove SRC labels');
-  }
-  
-  // success
-  return new Response('success');
+	// handle the return value of removeLabelsFromThread
+	if (!success) {
+		throw error(500, 'Failed to remove SRC labels');
+	}
+
+	// success
+	return new Response('success');
 };
