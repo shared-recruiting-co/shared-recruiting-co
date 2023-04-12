a9944bd5ae51d82f730ad59a02224b2db6956d32
Mark Steinbrick <95698968+msteinbrick@users.noreply.github.com>
2023-04-12 11:52:35 -0600

    Pagination Refactor (#251)
    * Pagination Refactor
    
    * properly use input url in building Pagination results
    
    * rename pagesDisplayArray to pagesDisplay
    
    * update Pagination import
    
    * get rid of hack, set pageUrl to null if invalid
    
    * update prev / next URL using searchParams.set()
    
    * fix spacing
    
    * remove console logs
    
    * add helper function for prev/next page url logic
    
    * use helper function to get updated page url in footer
    
    * remove quotes around templated strings
    
    * refactor helper function
    
    * address last review

--- web/src/lib/pagination.ts
+++ web/src/lib/pagination.ts
@@ -1,3 +1,107 @@
+const DEFAULT_PAGE_SIZE = 10;
+
+/**
+ * Helper function returns the url of the updated param page
+ *
+ * @param {string} currentPageUrl - The URL of the current page
+ * @param {number} updatedPage - The page number we would like to update to
+ * @param {boolean} updatedPageValid - Whether the updated page is valid or not
+ * @returns {string|null} - The updated page URL, or null if the updated page is not valid
+ */
+export const getUpdatedPageUrl = (currentPageUrl: string, updatedPage: number, updatedPageValid: boolean = true): string | null => {
+
+	// if the page is valid
+	if (updatedPageValid) {
+		
+		// create a new URL object from the current url
+		let updatedPageUrl = new URL(currentPageUrl) 
+
+		//return the udpate url
+		updatedPageUrl.searchParams.set('page', updatedPage);
+		return updatedPageUrl.href
+	} else {
+
+		// otherwise return null
+		return null
+	}
+}
+
+export type Pagination = {
+	currentResultsPage: number;
+	currentResultsPageUrl: URL;
+	resultsPerPage: number;
+	resultsToFetchStart: number;
+	resultsToFetchEnd: number;
+	resultShowingFirst: number;
+	resultShowingLast: number;
+	resultsCount: number;
+	pagesDisplay: Array<string>;
+	pagesCount: number;
+	prevPageValid: boolean;
+	prevPageUrl: string | null;
+	nextPageValid: boolean;
+	nextPageUrl: string | null;
+};
+
+/**
+ * Returns an object representing pagination information for the given inputs
+ *
+ * @param {URL} url - The URL of the current page
+ * @param {number} resultsCount - The total number of results fetched from the DB
+ * @param {number} resultsPerPage - The number of results to display per page (default above)
+ * @returns {Pagination} - An object representing pagination information for the search results
+ */
+export const getPagePagination = (url: URL, resultsCount: number, resultsPerPage = DEFAULT_PAGE_SIZE ): Pagination => {
+
+	// from the given URL, grab which page of results is currently selected (default 1)
+	const currentResultsPage = parseInt(url.searchParams.get('page')) || 1;
+
+	// define the start / stop range of rows we want to retreive from the DB for the page
+	const resultsToFetchStart = (currentResultsPage - 1) * resultsPerPage;
+	const resultsToFetchEnd = resultsToFetchStart + resultsPerPage;
+
+	// of the results, the first / last displayed on the page
+	const resultShowingFirst = (currentResultsPage - 1) * resultsPerPage + 1;
+	const resultShowingLast = Math.min(
+		currentResultsPage * resultsPerPage,
+		resultsCount
+	);
+
+	// the total number of pages
+	const pagesCount = Math.ceil(resultsCount / resultsPerPage)
+
+	// determine, based on the current page, if the prev / next pages are valid
+	const prevPageValid = (currentResultsPage > 1);
+	const nextPageValid = (currentResultsPage < pagesCount);
+
+	// if the prev / next page are valid, store what page they should correspond to
+	let prevPageUrl = getUpdatedPageUrl(url.href, currentResultsPage - 1, prevPageValid);
+	let nextPageUrl = getUpdatedPageUrl(url.href, currentResultsPage + 1, nextPageValid);
+
+	// get the array that will be displayed as the pages are select
+	const pagesDisplay = getPaginationPages(currentResultsPage, pagesCount);
+
+	// construct the pagination object
+	const pagination: Pagination = {
+		currentResultsPage: currentResultsPage,
+		currentResultsPageUrl: url.href,
+		resultsPerPage: resultsPerPage,
+		resultsToFetchStart: resultsToFetchStart,
+		resultsToFetchEnd: resultsToFetchEnd,
+		resultShowingFirst: resultShowingFirst,
+		resultShowingLast: resultShowingLast,
+		resultsCount: resultsCount,
+		pagesDisplay: pagesDisplay,
+		pagesCount: pagesCount,
+		prevPageValid: prevPageValid,
+		prevPageUrl: prevPageUrl,
+		nextPageValid: nextPageValid,
+		nextPageUrl: nextPageUrl
+	  };
+	
+	return pagination;
+};
+
 export const getPaginationPages = (current: number, total: number): string[] => {
 	if (total <= 5) {
 		return Array.from({ length: total }, (_, i) => (i + 1).toString());
--- web/src/routes/(app)/account/jobs/+page.server.ts
+++ web/src/routes/(app)/account/jobs/+page.server.ts
@@ -1,5 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { redirect, error } from '@sveltejs/kit';
+import { getPagePagination } from '$lib/pagination';
+import type { Pagination } from '$lib/pagination';
+
+
 
 import type { JobInterest } from '$lib/supabase/client';
 
@@ -14,15 +18,6 @@ type Job = {
 	recruiter_email: string;
 	emailed_at: string;
 	is_verified: boolean;
-};
-
-type Pagination = {
-	page: number;
-	perPage: number;
-	numPages: number;
-	numResults: number;
-	hasNext: boolean;
-	hasPrev: boolean;
 };
 
 type Data = {
@@ -42,45 +37,35 @@ export const load: PageServerLoad<Data> = async ({
 		throw redirect(303, '/login');
 	}
 
-	// get the query parameters from the URL (default to 1)
-	const page = parseInt(url.searchParams.get('page') || '1') || 1;
-	const start = (page - 1) * PAGE_SIZE;
-	const stop = start + PAGE_SIZE;
+	// retreive the total count of the users jobs
+	const { count, countError } = await	supabaseAdmin
+		.from('vw_job_board')
+		.select('*', {
+			head: true,
+			count: 'exact'
+		})
+		.eq('user_id', session.user.id)
 
-	// get jobs from database
-	// HACK:
-	// Use supabaseAdmin as a workaround for RLS not allowing candidates to see recruiter's name, email, and company info
-	const [{ data: jobs, jobsError }, { count, countError }] = await Promise.all([
-		supabaseAdmin
-			.from('vw_job_board')
-			.select('*')
-			.eq('user_id', session.user.id)
-			.order('emailed_at', { ascending: false })
-			.range(start, stop),
-		supabaseAdmin
-			.from('vw_job_board')
-			.select('*', {
-				head: true,
-				count: 'exact'
-			})
-			.eq('user_id', session.user.id)
-	]);
-	if (jobsError) {
-		throw error(500, jobsError.message);
-	}
 	if (countError || count === null) {
 		throw error(500, countError?.message);
 	}
 
-	return {
-		jobs,
-		pagination: {
-			page,
-			perPage: PAGE_SIZE,
-			numPages: Math.ceil(count / PAGE_SIZE),
-			numResults: count,
-			hasPrev: page > 1,
-			hasNext: page < Math.ceil(count / PAGE_SIZE)
-		}
-	};
+	// get pagination results 
+	const pagination: Pagination = getPagePagination(url, count);
+
+	// get jobs from database
+	// HACK:
+	// Use supabaseAdmin as a workaround for RLS not allowing candidates to see recruiter's name, email, and company info
+	const { data: jobs, jobsError } = await supabaseAdmin
+			.from('vw_job_board')
+			.select('*')
+			.eq('user_id', session.user.id)
+			.order('emailed_at', { ascending: false })
+			.range(pagination.resultsToFetchStart, pagination.resultsToFetchEnd)
+
+	if (jobsError) {
+		throw error(500, jobsError.message);
+	}
+
+	return {jobs, pagination};
 };
